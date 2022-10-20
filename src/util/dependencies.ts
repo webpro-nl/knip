2ae97ec0e7115fda7bbefba106dc073bb0c234a4
Lars Kappert <lars@webpro.nl>
2022-10-20 17:21:50 +0200

    Housekeeping the unresolved dependency analyzer

--- src/util/dependencies.ts
+++ src/util/dependencies.ts
@@ -1,11 +1,29 @@
 import { ts } from 'ts-morph';
 import isBuiltinModule from 'is-builtin-module';
 import micromatch from 'micromatch';
+import { findCallExpressionsByName } from 'ts-morph-helpers';
 import type { SourceFile } from 'ts-morph';
 import type { Configuration, Issue } from '../types';
 
 const compact = <T>(collection: (T | undefined)[]) =>
   Array.from(new Set(collection)).filter((value): value is T => Boolean(value));
+
+const findRequireModuleSpecifiers = (sourceFile: SourceFile) =>
+  findCallExpressionsByName(sourceFile, 'require').map(expression =>
+    expression.getFirstDescendantByKind(ts.SyntaxKind.StringLiteral)
+  );
+
+const isExternalDependency = (moduleSpecifier: string, tsConfigPaths: string[]) => {
+  if (moduleSpecifier.startsWith('.')) return false;
+  if (isBuiltinModule(moduleSpecifier)) return false;
+  if (tsConfigPaths.length > 0 && micromatch.isMatch(moduleSpecifier, tsConfigPaths)) return false;
+  return true;
+};
+
+const resolvePackageName = (moduleSpecifier: string) => {
+  const parts = moduleSpecifier.split('/').slice(0, 2);
+  return moduleSpecifier.startsWith('@') ? parts.join('/') : parts[0];
+};
 
 export const getDependencyAnalyzer = (configuration: Configuration) => {
   const { dependencies, devDependencies, peerDependencies, optionalDependencies, tsConfigPaths } = configuration;
@@ -16,20 +34,17 @@ export const getDependencyAnalyzer = (configuration: Configuration) => {
 
   const getUnresolvedDependencies = (sourceFile: SourceFile) => {
     const unresolvedDependencies: Set<Issue> = new Set();
+
     // Unfortunately `sourceFile.getReferencedSourceFiles` does not seem to return `require` calls, so here we are.
     const importLiterals = sourceFile.getImportStringLiterals();
-    const requires = sourceFile
-      .getDescendantsOfKind(ts.SyntaxKind.CallExpression)
-      .filter(callExpression => callExpression.getExpression().getText() === 'require')
-      .map(expression => expression.getFirstDescendantByKind(ts.SyntaxKind.StringLiteral));
-    const literals = compact([importLiterals, requires].flat());
-    literals.forEach(importLiteral => {
-      const moduleSpecifier = importLiteral.getLiteralText();
-      if (moduleSpecifier.startsWith('.')) return;
-      if (isBuiltinModule(moduleSpecifier)) return;
-      if (tsConfigPaths.length > 0 && micromatch.isMatch(moduleSpecifier, tsConfigPaths)) return;
-      const parts = moduleSpecifier.split('/').slice(0, 2);
-      const packageName = moduleSpecifier.startsWith('@') ? parts.join('/') : parts[0];
+    const requireCallExpressions = findRequireModuleSpecifiers(sourceFile);
+    const moduleSpecifiers = compact([importLiterals, requireCallExpressions].flat()).map(i => i.getLiteralText());
+
+    moduleSpecifiers.forEach(moduleSpecifier => {
+      if (!isExternalDependency(moduleSpecifier, tsConfigPaths)) return;
+
+      const packageName = resolvePackageName(moduleSpecifier);
+
       if (!productionDependencies.includes(packageName) && !devDependencies.includes(packageName)) {
         unresolvedDependencies.add({ filePath: sourceFile.getFilePath(), symbol: moduleSpecifier });
       }
