20f0b7ca8f1a2c4da5e4c86d0acd1046450fb2c7
Lars Kappert <lars@webpro.nl>
2022-12-16 10:35:02 +0100

    Extend/improve webpack plugin

--- src/plugins/webpack/index.ts
+++ src/plugins/webpack/index.ts
@@ -2,7 +2,7 @@ import { compact } from '../../util/array.js';
 import { _load } from '../../util/loader.js';
 import { timerify } from '../../util/performance.js';
 import type { IsPluginEnabledCallback, GenericPluginCallback } from '../../types/plugins.js';
-import type { Configuration } from 'webpack';
+import type { Configuration, RuleSetRule, RuleSetUseItem } from 'webpack';
 
 // https://webpack.js.org/configuration/
 
@@ -12,28 +12,32 @@ export const ENTRY_FILE_PATTERNS = ['webpack.config*.js'];
 
 export const isEnabled: IsPluginEnabledCallback = ({ dependencies }) => dependencies.has('webpack');
 
-const findWebpackDependencies: GenericPluginCallback = async configFilePath => {
+const resolveRuleSetLoaders = (rule: RuleSetRule | '...') => {
+  if (!rule || typeof rule === 'string') return [];
+  if (typeof rule.use === 'string') return [rule.use];
+  if (Array.isArray(rule.use)) return rule.use.flatMap(resolveUseItemLoader);
+  return rule.use && typeof rule.use !== 'function' ? resolveUseItemLoader(rule.use) : [];
+};
+
+const resolveUseItemLoader = (use: RuleSetUseItem) => {
+  if (!use) return [];
+  if (typeof use === 'string') return [use];
+  if ('loader' in use && typeof use.loader === 'string') return [use.loader];
+  return [];
+};
+
+const findWebpackDependencies: GenericPluginCallback = async (configFilePath, { manifest }) => {
   const config: Configuration = await _load(configFilePath);
 
-  const loaders = (
-    config.module?.rules?.flatMap(rule => {
-      if (!rule || typeof rule === 'string') return [];
-      if (typeof rule.use === 'string') return [rule.use];
-      if (Array.isArray(rule.use)) {
-        return rule.use.flatMap(use => {
-          if (!use) return [];
-          if (typeof use === 'string') return [use];
-          if ('loader' in use && typeof use.loader === 'string') return [use.loader];
-          return [];
-        });
-      }
-      return [];
-    }) ?? []
-  )
+  const loaders = (config.module?.rules?.flatMap(resolveRuleSetLoaders) ?? [])
     .map(loader => loader.replace(/\?.*/, ''))
     .filter(loader => !loader.startsWith('/'));
 
-  return compact([...loaders]);
+  const scripts = Object.values(manifest.scripts ?? {});
+  const webpackCLI = scripts.some(script => script?.includes('webpack ')) ? ['webpack-cli'] : [];
+  const webpackDevServer = scripts.some(script => script?.includes('webpack serve')) ? ['webpack-dev-server'] : [];
+
+  return compact([...loaders, ...webpackCLI, ...webpackDevServer]);
 };
 
 export const findDependencies = timerify(findWebpackDependencies);
