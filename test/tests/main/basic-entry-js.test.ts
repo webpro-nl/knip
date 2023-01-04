745dc65c2f80091d133c4091e507d49dd1f14d1a
Lars Kappert <lars@webpro.nl>
2023-01-04 15:41:47 +0100

    Move test files

---
+++ test/tests/main/basic-entry-js.test.ts
@@ -0,0 +1,45 @@
+import assert from 'node:assert/strict';
+import test from 'node:test';
+import { main } from '../../../src/index.js';
+import baseArguments from '../../helpers/baseArguments.js';
+import baseCounters from '../../helpers/baseCounters.js';
+
+test('Find unused files and exports with JS entry file', async () => {
+  const cwd = 'test/fixtures/entry-js';
+
+  const { issues, counters } = await main({
+    ...baseArguments,
+    cwd,
+  });
+
+  assert.equal(issues.files.size, 1);
+  assert(Array.from(issues.files)[0].endsWith('dangling.js'));
+
+  assert.equal(Object.values(issues.exports).length, 1);
+  assert.equal(issues.exports['my-module.js']['unused'].symbol, 'unused');
+
+  assert.equal(Object.values(issues.types).length, 1);
+  assert.equal(issues.types['my-module.js']['AnyType'].symbolType, 'type');
+
+  assert.equal(Object.values(issues.nsExports).length, 1);
+  assert.equal(issues.nsExports['my-namespace.ts']['key'].symbol, 'key');
+
+  assert.equal(Object.values(issues.nsTypes).length, 1);
+  assert.equal(issues.nsTypes['my-namespace.ts']['MyNamespace'].symbol, 'MyNamespace');
+
+  assert.equal(Object.values(issues.duplicates).length, 1);
+  assert.equal(issues.duplicates['my-module.js']['myExport|default'].symbols?.[0], 'myExport');
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    files: 1,
+    unlisted: 0,
+    exports: 1,
+    nsExports: 1,
+    nsTypes: 1,
+    types: 1,
+    duplicates: 1,
+    processed: 4,
+    total: 4,
+  });
+});
---
+++ test/tests/plugins/nyc.test.ts
@@ -0,0 +1,12 @@
+import assert from 'node:assert/strict';
+import path from 'node:path';
+import test from 'node:test';
+import * as nyc from '../../../src/plugins/nyc/index.js';
+
+const cwd = path.resolve('test/fixtures/plugins/nyc');
+
+test('Find dependencies in nyc configuration', async () => {
+  const configFilePath = path.join(cwd, '.nycrc.json');
+  const dependencies = await nyc.findDependencies(configFilePath);
+  assert.deepEqual(dependencies, ['@istanbuljs/nyc-config-typescript']);
+});
