145aa9cb6200d8ad4663715237e9c28ed25f3aca
Lars Kappert <lars@webpro.nl>
2023-01-06 02:35:47 +0100

    Improve create-plugin script and add test + fixture dir

--- scripts/create-new-plugin.ts
+++ scripts/create-new-plugin.ts
@@ -15,8 +15,8 @@ if (!name) {
   process.exit(1);
 }
 
-if (/[^a-z]/.test(name)) {
-  console.error('Name must contain only lowercased letters (you can adjust the plugin title later)');
+if (/[^a-z_-]/.test(name)) {
+  console.error('Name must contain only lowercased letters, dashes and underscores (you can adjust the title later)');
   process.exit(1);
 }
 
@@ -26,6 +26,13 @@ const templateDir = path.join(pluginsDir, '_template');
 const newPluginDir = path.join(pluginsDir, name);
 const pluginsBarrelFilePath = path.join(pluginsDir, 'index.ts');
 const schemaFilePath = path.join(cwd, 'schema.json');
+const pluginTestsDir = path.join(cwd, 'test/tests/plugins');
+const pluginTestTemplateFilePath = path.join(pluginTestsDir, '_template.test.ts');
+const pluginTestFilePath = path.join(pluginTestsDir, `${name}.test.ts`);
+const pluginTestFixturesDir = path.join(cwd, 'test/fixtures/plugins');
+const pluginTestFixtureTemplateDir = path.join(pluginTestFixturesDir, '_template');
+const pluginTestFixturePluginDir = path.join(pluginTestFixturesDir, name);
+const camelCasedName = name.toLowerCase().replace(/(-[a-z])/g, group => group.toUpperCase().replace('-', ''));
 
 const relative = to => path.relative(cwd, to);
 
@@ -37,8 +44,16 @@ await fs.cp(templateDir, newPluginDir, {
 
 await fs.writeFile(
   pluginsBarrelFilePath,
-  String(await fs.readFile(pluginsBarrelFilePath)) + `export * as ${name} from './${name}/index.js';`
+  String(await fs.readFile(pluginsBarrelFilePath)) + `export * as ${camelCasedName} from './${name}/index.js';`
 );
+
+await fs.cp(pluginTestFixtureTemplateDir, pluginTestFixturePluginDir, {
+  recursive: true,
+  errorOnExist: true,
+  force: false,
+});
+
+await fs.copyFile(pluginTestTemplateFilePath, pluginTestFilePath);
 
 const { default: schema } = await import(schemaFilePath);
 const { plugins } = schema.definitions;
@@ -56,6 +71,7 @@ plugins.properties = Object.keys(properties)
 await fs.writeFile(schemaFilePath, JSON.stringify(schema, null, 2));
 
 console.log(`Created new plugin at ${relative(newPluginDir)}`);
-console.log(`Updated ${relative(newPluginDir)} and ${relative(schemaFilePath)}, please review the changes`);
-console.log('Please populate index.ts with configuration and implementation (see the other plugins for inspiration!)');
-console.log('The README.md can then be generated using `npm run docs:plugins`');
+console.log(`Updated ${relative(pluginsBarrelFilePath)} and ${relative(schemaFilePath)}, please review the changes`);
+console.log(`Created a test file at ${relative(pluginTestFilePath)}`);
+console.log('Please review the changes, populate index.ts with config and implementation, and update the test(s)');
+console.log('See the other plugins for inspiration! The README.md can then be generated using `npm run docs:plugins`');
