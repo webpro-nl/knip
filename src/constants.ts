e509553c7252ab85b5077cd1e7c81b7993cea046
Lars Kappert <lars@webpro.nl>
2023-01-19 12:20:33 +0100

    Add some binaries to IGNORED_GLOBAL_BINARIES

--- src/constants.ts
+++ src/constants.ts
@@ -15,9 +15,25 @@ export const DEFAULT_WORKSPACE_CONFIG = {
 // This is what gets excluded in --production mode (apart from what plugins would include)
 export const TEST_FILE_PATTERNS = ['**/*.{test,spec}.{js,jsx,ts,tsx}', '**/__tests__/**/*.{js,jsx,ts,tsx}'];
 
-// Binaries that are expected to be globally installed when using a certain project
-// This assumes for each of them that https://www.npmjs.com/package/[name] is NOT the expected dependency.
-export const IGNORED_GLOBAL_BINARIES = ['npm', 'npx', 'node', 'yarn', 'pnpm', 'deno', 'git'];
+// Binaries that are expected to be globally installed (i.e. https://www.npmjs.com/package/[name] is NOT the expected dependency)
+export const IGNORED_GLOBAL_BINARIES = [
+  'deno',
+  'git',
+  'node',
+  'npm',
+  'npx',
+  'pnpm',
+  'yarn',
+  // Packages exist, but are at least 6 years old:
+  'cd',
+  'cp',
+  'echo',
+  'exit',
+  'mkdir',
+  'mv',
+  'rm',
+  'sudo',
+];
 
 // The `@types/node` dependency does not require the `node` dependency
 export const IGNORE_DEFINITELY_TYPED = ['node'];
