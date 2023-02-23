0920366d90aad26aef97489e4e7fc9ca898bf72a
Lars Kappert <lars@webpro.nl>
2023-02-23 10:11:33 +0100

    Load .mjs directly through import call

--- src/util/loader.ts
+++ src/util/loader.ts
@@ -1,5 +1,6 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
+import { pathToFileURL } from 'node:url';
 // eslint-disable-next-line import/order -- Modules in @types are handled differently
 import { load as esmLoad } from '@esbuild-kit/esm-loader';
 import yaml from 'js-yaml';
@@ -10,12 +11,19 @@ import { timerify } from './performance.js';
 
 const load = async (filePath: string) => {
   try {
-    if (/\.jsonc?$/.test(filePath) || /rc$/.test(filePath)) {
+    const ext = path.extname(filePath);
+    if (ext === '.json' || ext === '.jsonc' || /rc$/.test(filePath)) {
       return loadJSON(filePath);
     }
 
-    if (path.extname(filePath) === '.yaml' || path.extname(filePath) === '.yml') {
+    if (ext === '.yaml' || ext === '.yml') {
       return yaml.load((await fs.readFile(filePath)).toString());
+    }
+
+    if (ext === '.mjs') {
+      const fileUrl = pathToFileURL(filePath);
+      const imported = await import(fileUrl.href);
+      return imported.default ?? imported;
     }
 
     const imported = await esmLoad(filePath, {}, require);
