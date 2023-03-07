1235b04ce19facef0ea0a4fb6bf9f074c1dcb096
Lars Kappert <lars@webpro.nl>
2023-03-07 20:49:29 +0100

    Improve workspace name sorter

--- src/util/workspace.ts
+++ src/util/workspace.ts
@@ -1,7 +1,9 @@
+// Paths sorter predicate function, starting with "root", ends with "the/deep/est/path"
 export const byPathDepth = (a: string, b: string) => {
   const depthA = a.split('/');
   const depthB = b.split('/');
   if (depthA.length !== depthB.length) return depthA.length - depthB.length;
   if (depthA.includes('*') || depthA.includes('**')) return -1;
+  if (depthB.includes('*') || depthB.includes('**')) return 1;
   return a.length - b.length;
 };
