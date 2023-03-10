e933aea94824e3d9fecdc8394b2913685b45548e
Lars Kappert <lars@webpro.nl>
2023-03-10 09:22:50 +0100

    Fix up and rename tsconfig-paths-implicit test

--- tests/main/tsconfig-paths-implicit.test.ts
+++ tests/main/tsconfig-paths-implicit.test.ts
@@ -6,7 +6,7 @@ import baseArguments from '../helpers/baseArguments.js';
 import baseCounters from '../helpers/baseCounters.js';
 
 test('Resolve modules properly using tsconfig paths with baseUrl', async () => {
-  const cwd = resolve('tests/fixtures/tsconfig-paths-baseurl');
+  const cwd = resolve('tests/fixtures/tsconfig-paths-implicit');
 
   const { counters } = await main({
     ...baseArguments,
