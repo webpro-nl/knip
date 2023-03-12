1eb84dcbbbae08710db7e01ab520946455c78088
Lars Kappert <lars@webpro.nl>
2023-03-12 09:34:02 +0100

    Add test for workspaces-paths fixture

---
+++ tests/main/workspaces-paths.test.ts
@@ -0,0 +1,24 @@
+import assert from 'node:assert/strict';
+import test from 'node:test';
+import { main } from '../../src/index.js';
+import { resolve } from '../../src/util/path.js';
+import baseArguments from '../helpers/baseArguments.js';
+import baseCounters from '../helpers/baseCounters.js';
+
+test('Find unused files, dependencies and exports in workspaces (w/ paths)', async () => {
+  const cwd = resolve('tests/fixtures/workspaces-paths');
+  const { issues, counters } = await main({
+    ...baseArguments,
+    cwd,
+  });
+
+  assert.equal(Object.keys(issues.unlisted).length, 1);
+  assert(issues.unlisted['packages/lib-e/src/index.ts']['not/found']);
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    unlisted: 1,
+    processed: 11,
+    total: 11,
+  });
+});
