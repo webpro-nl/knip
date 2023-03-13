2de73d101d3b725b88e4d458a95d85c8de2e41fb
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2023-03-13 13:58:18 +1300

    refactor: tidy up

--- src/plugins/semantic-release/index.ts
+++ src/plugins/semantic-release/index.ts
@@ -4,7 +4,7 @@ import { hasDependency } from '../../util/plugin.js';
 import type { PluginConfig } from './types.js';
 import type { IsPluginEnabledCallback, GenericPluginCallback } from '../../types/plugins.js';
 
-// link to docs
+// https://github.com/semantic-release/semantic-release/blob/master/docs/usage/configuration.md#configuration-file
 
 export const NAME = 'Semantic Release';
 
@@ -13,19 +13,12 @@ export const ENABLERS = ['semantic-release'];
 
 export const isEnabled: IsPluginEnabledCallback = ({ dependencies }) => hasDependency(dependencies, ENABLERS);
 
-// https://github.com/semantic-release/semantic-release/blob/master/docs/usage/configuration.md#configuration-file
 export const CONFIG_FILE_PATTERNS = [
   '.releaserc',
   '.releaserc.{yaml,yml,json,js,cjs}',
   'release.config.{js,cjs}',
   'package.json',
 ];
-
-export const ENTRY_FILE_PATTERNS = [];
-
-export const PRODUCTION_ENTRY_FILE_PATTERNS = [];
-
-export const PROJECT_FILE_PATTERNS = [];
 
 const findPluginDependencies: GenericPluginCallback = async (configFilePath, { manifest }) => {
   const config: PluginConfig = configFilePath.endsWith('package.json') ? manifest.release : await _load(configFilePath);
