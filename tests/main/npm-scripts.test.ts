b4b5f257ddb3d83a495ab8195ab1212b82b1ff21
Lars Kappert <lars@webpro.nl>
2023-03-13 17:46:04 +0100

    Merge npm-scripts tests

--- tests/main/npm-scripts.test.ts
+++ tests/main/npm-scripts.test.ts
@@ -1,11 +1,56 @@
 import assert from 'node:assert/strict';
 import test from 'node:test';
 import { main } from '../../src/index.js';
+import * as npm from '../../src/manifest/index.js';
 import { resolve } from '../../src/util/path.js';
 import baseArguments from '../helpers/baseArguments.js';
 import baseCounters from '../helpers/baseCounters.js';
+import { getManifest } from '../helpers/index.js';
 
 const cwd = resolve('tests/fixtures/npm-scripts');
+const manifest = getManifest(cwd);
+
+test('Referenced dependencies in npm scripts', async () => {
+  const config = {
+    config: {
+      include: [],
+      exclude: [],
+      workspaces: {},
+      ignore: [],
+      ignoreBinaries: ['bash', 'rm'],
+      ignoreDependencies: [],
+      ignoreWorkspaces: [],
+    },
+    manifest,
+    isRoot: true,
+    isProduction: false,
+    isStrict: false,
+    dir: cwd,
+    cwd,
+  };
+
+  const { dependencies, peerDependencies, installedBinaries } = await npm.findDependencies(config);
+
+  assert.deepEqual(dependencies, ['nodemon', 'dotenv', 'nx', 'pm2', 'eslint', 'peer-dep']);
+
+  const expectedPeerDependencies = new Map();
+  expectedPeerDependencies.set('pm2-peer-dep', new Set(['pm2']));
+
+  assert.deepEqual(peerDependencies, expectedPeerDependencies);
+
+  assert.deepEqual(
+    installedBinaries,
+    new Map([
+      ['pm2', new Set(['pm2'])],
+      ['pm2-dev', new Set(['pm2'])],
+      ['pm2-docker', new Set(['pm2'])],
+      ['pm2-runtime', new Set(['pm2'])],
+      ['nx', new Set(['nx'])],
+      ['unused', new Set(['unused'])],
+      ['eslint', new Set(['eslint', 'eslint-v6', 'eslint-v7', 'eslint-v8'])],
+    ])
+  );
+});
 
 test('Unused dependencies in npm scripts', async () => {
   const { issues, counters } = await main({
