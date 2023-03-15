0ae6fd87340d8171b20e68104ccb0e10eb5801c2
Lars Kappert <lars@webpro.nl>
2023-03-15 19:55:58 +0100

    Revisit the svelte fixture

--- tests/fixtures/plugins/svelte/knip.ts
+++ tests/fixtures/plugins/svelte/knip.ts
@@ -2,7 +2,6 @@ export default {
   ignore: ['.svelte-kit', '**/*.d.ts'],
   paths: {
     '$app/*': ['node_modules/@sveltejs/kit/src/runtime/app/*'],
-    '$lib/*': ['src/lib/*'],
   },
   compilers: {
     css: (text: string) => [...text.matchAll(/(?<=@)import[^;]+/g)].join('\n'),
