b9495c44a0d37508208b9db37c55a5ce60c3a924
Lars Kappert <lars@webpro.nl>
2023-03-16 16:39:34 +0100

    Add workspaces-self-references test and fixtures

---
+++ tests/fixtures/workspaces-self-reference/packages/eslint-config-self-reference/entry.ts
@@ -0,0 +1,5 @@
+import index from '@workspaces-self-reference/eslint-config-self-reference';
+import build from '@workspaces-self-reference/eslint-config-self-reference/build';
+import local from '@workspaces-self-reference/self-reference-from-plugin/local';
+
+index; build; local;
