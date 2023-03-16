db5a35e260dc0df518278a861a6a0f6fc6e55d3c
Lars Kappert <lars@webpro.nl>
2023-03-16 23:50:05 +0100

    Revert "Suppress issues in tests for Windows"
    This reverts commit cd6fe79ee9639bdce7f927111a5f97ccf23585ec.

--- tests/workspaces-self-reference.test.ts
+++ tests/workspaces-self-reference.test.ts
@@ -4,7 +4,6 @@ import { main } from '../src/index.js';
 import { resolve } from '../src/util/path.js';
 import baseArguments from './helpers/baseArguments.js';
 import baseCounters from './helpers/baseCounters.js';
-import { isWindows } from './helpers/index.js';
 
 test('Find unused files, dependencies and exports in workspaces with cross self-references', async () => {
   const cwd = resolve('tests/fixtures/workspaces-self-reference');
@@ -16,7 +15,7 @@ test('Find unused files, dependencies and exports in workspaces with cross self-
 
   assert.deepEqual(counters, {
     ...baseCounters,
-    processed: isWindows ? 5 : 6, // TODO
-    total: isWindows ? 5 : 6, // TODO
+    processed: 6,
+    total: 6,
   });
 });
