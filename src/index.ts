webpro-nl/knip

commit 3f223293783ed127ece07fcf1ed1468eb296878d

Use ts.parseJsonConfigFileContent to load extended TS configs (fixes #75, related to #74)

src/principal-factory.ts
@@ -22,7 +22,9 @@ const mergePaths = (cwd: string, compilerOptions: ts.CompilerOptions = {}, paths
   const baseUrl = compilerOptions.baseUrl ?? '.';
   compilerOptions.paths = Object.keys(mergedPaths).reduce((paths, key) => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    paths![key] = mergedPaths[key].map(entry => (isAbsolute(entry) ? entry : join(cwd, baseUrl, entry)));
+    paths![key] = mergedPaths[key].map(entry =>
+      isAbsolute(entry) ? entry : isAbsolute(baseUrl) ? join(baseUrl, entry) : join(cwd, baseUrl, entry)
+    );
     return paths;
   }, {} as Paths);
   return compilerOptions;

tests/fixtures/tsconfig-paths-extends/src/index.ts
@@ -1,0 +1,4 @@
+import main from '~/lib/main';
+import util from '~/util/index';
+
+export { main, util }

tests/fixtures/tsconfig-paths-extends/src/util/index.ts
@@ -1,0 +1,1 @@
+export default 1;

src/util/tsconfig-loader.ts
@@ -1,9 +1,12 @@
+import path from 'node:path';
 import ts from 'typescript';
 import { isFile } from './fs.js';
 
 export const loadTSConfig = async (tsConfigFilePath: string) => {
   if (isFile(tsConfigFilePath)) {
     const config = ts.readConfigFile(tsConfigFilePath, ts.sys.readFile);
-    return config.config.compilerOptions;
+    const parsedConfig = ts.parseJsonConfigFileContent(config.config, ts.sys, path.dirname(tsConfigFilePath));
+    return parsedConfig.options ?? {};
   }
+  return {};
 };

tests/fixtures/tsconfig-paths-extends/src/cli.ts
@@ -1,0 +1,4 @@
+import { main,util } from '~/main';
+
+main;
+util;

tests/fixtures/tsconfig-paths-extends/src/lib/main.ts
@@ -1,0 +1,1 @@
+export default 1;

tests/main/tsconfig-paths-extends.test.ts
@@ -1,0 +1,21 @@
+import assert from 'node:assert/strict';
+import test from 'node:test';
+import { main } from '../../src/index.js';
+import { resolve } from '../../src/util/path.js';
+import baseArguments from '../helpers/baseArguments.js';
+import baseCounters from '../helpers/baseCounters.js';
+
+test('Resolve modules properly using tsconfig paths from extended config', async () => {
+  const cwd = resolve('tests/fixtures/tsconfig-paths-extends');
+
+  const { counters } = await main({
+    ...baseArguments,
+    cwd,
+  });
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    processed: 4,
+    total: 4,
+  });
+});

commit dc63cc2c4961a05c39303a22631592910c4cc261

Fix principal reusage (regressed with 3f223293783ed127ece07fcf1ed1468eb296878d)

src/index.ts
@@ -6,7 +6,7 @@ import { IssueCollector } from './issue-collector.js';
 import { PrincipalFactory } from './principal-factory.js';
 import { Exports, ImportedModule, Imports } from './types/ast.js';
 import { compact } from './util/array.js';
-import { debugLogObject, debugLogArray } from './util/debug.js';
+import { debugLogObject, debugLogArray, debugLog } from './util/debug.js';
 import { ConfigurationError } from './util/errors.js';
 import { findFile } from './util/fs.js';
 import { _glob } from './util/glob.js';
@@ -187,6 +187,8 @@ export const main = async (unresolvedConfiguration: CommandLineOptions) => {
 
   const principals = factory.getPrincipals();
 
+  debugLog(`Installed ${principals.length} principals for ${workspaces.length} workspaces`);
+
   for (const principal of principals) {
     const exportedSymbols: Exports = new Map();
     const importedSymbols: Imports = new Map();
@@ -272,7 +274,7 @@ export const main = async (unresolvedConfiguration: CommandLineOptions) => {
       const resolvedFiles = principal.getUsedResolvedFiles();
       const files = resolvedFiles.filter(filePath => !analyzedFiles.has(filePath));
 
-      debugLogArray(`Analyzing used resolved files [${++round}/${principals.indexOf(principal) + 1}]`, files);
+      debugLogArray(`Analyzing used resolved files [P${principals.indexOf(principal) + 1}/${++round}]`, files);
       files.forEach(filePath => {
         analyzeSourceFile(filePath);
         analyzedFiles.add(filePath);

src/principal-factory.ts
@@ -6,7 +6,7 @@ import type { Report } from './types/issues.js';
 
 type Paths = ts.CompilerOptions['paths'];
 
-type Principal = { principal: ProjectPrincipal; cwds: Set<string>; paths: Set<string>; isDefaultBaseUrl: boolean };
+type Principal = { principal: ProjectPrincipal; cwds: Set<string>; paths: Set<string> };
 type Principals = Set<Principal>;
 
 type Options = {
@@ -30,9 +30,6 @@ const mergePaths = (cwd: string, compilerOptions: ts.CompilerOptions = {}, paths
   return compilerOptions;
 };
 
-const hasDefaultBaseUrl = (compilerOptions: ts.CompilerOptions) =>
-  !compilerOptions.baseUrl || /^\.\/?$/.test(compilerOptions.baseUrl);
-
 /**
  * The principal factory hands out ProjectPrincipals. It tries to reuse them, since they're expensive in terms of
  * performance. Time will tell if this is actually feasible or not.
@@ -52,17 +49,16 @@ export class PrincipalFactory {
   }
 
   /**
-   * Principals (or rather their `compilerOptions`) are considered reusable when:
-   *
-   * - It does not have any keys in compiler.paths in common
-   * - It does not have a (non-default) `baseUrl`
+   * Principals with shared `compilerOptions.baseUrl` and no `compilerOptions.paths` conflicts are reused.
    */
   private findReusablePrincipal(compilerOptions: ts.CompilerOptions) {
-    if (!hasDefaultBaseUrl(compilerOptions)) return;
     const workspacePaths = compilerOptions?.paths ? Object.keys(compilerOptions.paths) : [];
-    const principal = Array.from(this.principals).find(
-      principal => principal.isDefaultBaseUrl && workspacePaths.every(p => !principal.paths.has(p))
-    );
+    const principal = Array.from(this.principals).find(principal => {
+      if (compilerOptions.baseUrl === principal.principal.compilerOptions.baseUrl) {
+        return workspacePaths.every(p => !principal.paths.has(p));
+      }
+      return !compilerOptions.baseUrl;
+    });
     return principal;
   }
 
@@ -75,9 +71,8 @@ export class PrincipalFactory {
   private addNewPrincipal({ cwd, compilerOptions, report, compilers }: Options) {
     const principal = new ProjectPrincipal({ cwd, compilerOptions, report, compilers });
     const paths = new Set(Object.keys(compilerOptions?.paths ?? {}));
-    const isDefaultBaseUrl = hasDefaultBaseUrl(compilerOptions);
     compilerOptions.baseUrl = join(cwd, compilerOptions.baseUrl ?? '.');
-    this.principals.add({ principal, cwds: new Set([cwd]), paths, isDefaultBaseUrl });
+    this.principals.add({ principal, cwds: new Set([cwd]), paths });
     return principal;
   }
 