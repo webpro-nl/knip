522c8a75f548af7803929ccd4c679806a6a40725
Lars Kappert <lars@webpro.nl>
2023-03-28 16:10:39 +0200

    Fix npx binary resolver

--- src/binaries/resolvers/npx.ts
+++ src/binaries/resolvers/npx.ts
@@ -3,5 +3,7 @@ import type { Resolver } from '../types.js';
 
 export const resolve: Resolver = (binary, args, { fromArgs }) => {
   const parsed = parseArgs(args, { '--': true, stopEarly: true, boolean: ['yes', 'no'], alias: { yes: 'y', no: 'n' } });
-  return [...(parsed.yes ? [] : fromArgs(parsed._)), ...(parsed['--'] ? fromArgs(parsed['--']) : [])];
+  const left = fromArgs(parsed._);
+  const right = parsed['--'] ? fromArgs(parsed['--']) : [];
+  return [...(parsed.yes ? left.slice(1) : left), ...right];
 };
