325fca989f5fdd272f121ee5eef18158c65d9042
Lars Kappert <lars@webpro.nl>
2023-03-29 20:23:39 +0200

    Run require and require.resolve visitors in TS files too

--- src/typescript/visitors/imports/requireCall.ts
+++ src/typescript/visitors/imports/requireCall.ts
@@ -1,51 +1,53 @@
 import ts from 'typescript';
 import { isRequireCall, findAncestor, findDescendants } from '../../ast-helpers.js';
-import { isJS } from '../helpers.js';
 import { importVisitor as visit } from '../index.js';
 
-export default visit(isJS, node => {
-  if (isRequireCall(node)) {
-    if (ts.isStringLiteralLike(node.arguments[0])) {
-      const specifier = node.arguments[0].text;
+export default visit(
+  () => true,
+  node => {
+    if (isRequireCall(node)) {
+      if (ts.isStringLiteralLike(node.arguments[0])) {
+        const specifier = node.arguments[0].text;
 
-      if (specifier) {
-        const propertyAccessExpression = findAncestor<ts.PropertyAccessExpression>(node, _node => {
-          if (ts.isExpressionStatement(_node) || ts.isCallExpression(_node)) return 'STOP';
-          return ts.isPropertyAccessExpression(_node);
-        });
+        if (specifier) {
+          const propertyAccessExpression = findAncestor<ts.PropertyAccessExpression>(node, _node => {
+            if (ts.isExpressionStatement(_node) || ts.isCallExpression(_node)) return 'STOP';
+            return ts.isPropertyAccessExpression(_node);
+          });
 
-        if (propertyAccessExpression) {
-          // Pattern: require('side-effects').identifier
-          const identifier = String(propertyAccessExpression.name.escapedText);
-          return { identifier, specifier };
-        } else {
-          const variableDeclaration = node.parent;
-          if (
-            ts.isVariableDeclaration(variableDeclaration) &&
-            ts.isVariableDeclarationList(variableDeclaration.parent)
-          ) {
-            if (ts.isIdentifier(variableDeclaration.name)) {
-              // Pattern: identifier = require('specifier')
-              return { identifier: 'default', specifier };
-            } else {
-              const bindings = findDescendants<ts.BindingElement>(variableDeclaration, ts.isBindingElement);
-              if (bindings.length > 0) {
-                // Pattern: { identifier } = require('specifier')
-                return bindings.map(element => {
-                  const identifier = (element.propertyName ?? element.name).getText();
-                  return { identifier, specifier };
-                });
-              } else {
-                // Pattern: require('specifier')
-                return { identifier: 'default', specifier };
-              }
-            }
+          if (propertyAccessExpression) {
+            // Pattern: require('side-effects').identifier
+            const identifier = String(propertyAccessExpression.name.escapedText);
+            return { identifier, specifier };
           } else {
-            // Pattern: require('side-effects')
-            return { identifier: 'default', specifier };
+            const variableDeclaration = node.parent;
+            if (
+              ts.isVariableDeclaration(variableDeclaration) &&
+              ts.isVariableDeclarationList(variableDeclaration.parent)
+            ) {
+              if (ts.isIdentifier(variableDeclaration.name)) {
+                // Pattern: identifier = require('specifier')
+                return { identifier: 'default', specifier };
+              } else {
+                const bindings = findDescendants<ts.BindingElement>(variableDeclaration, ts.isBindingElement);
+                if (bindings.length > 0) {
+                  // Pattern: { identifier } = require('specifier')
+                  return bindings.map(element => {
+                    const identifier = (element.propertyName ?? element.name).getText();
+                    return { identifier, specifier };
+                  });
+                } else {
+                  // Pattern: require('specifier')
+                  return { identifier: 'default', specifier };
+                }
+              }
+            } else {
+              // Pattern: require('side-effects')
+              return { identifier: 'default', specifier };
+            }
           }
         }
       }
     }
   }
-});
+);
