2ac27fa84ebee3eaa9e2f38ecbdf3ae46949e803
Lars Kappert <lars@webpro.nl>
2023-04-11 12:24:41 +0200

    Add test for typescript plugin, and to assert trailing commas in JSON are fine

---
+++ tests/plugins/typescript.test.ts
@@ -0,0 +1,16 @@
+import assert from 'node:assert/strict';
+import test from 'node:test';
+import * as typescript from '../../src/plugins/typescript/index.js';
+import { resolve, join } from '../../src/util/path.js';
+
+const cwd = resolve('tests/fixtures/plugins/typescript');
+
+test('Find dependencies in typescript configuration (json)', async () => {
+  const configFilePath = join(cwd, 'tsconfig.json');
+  const dependencies = await typescript.findDependencies(configFilePath);
+  assert.deepEqual(dependencies, [
+    '@tsconfig/node16/tsconfig.json',
+    'typescript-eslint-language-service',
+    'ts-graphql-plugin',
+  ]);
+});
