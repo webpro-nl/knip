bf3fcbb1abdd80f6130d310fcca0081e86796004
Lars Kappert <lars@webpro.nl>
2023-04-23 18:07:27 +0200

    Fix import ext in test

--- tests/plugins/tailwind.test.ts
+++ tests/plugins/tailwind.test.ts
@@ -3,7 +3,7 @@ import test from 'node:test';
 import { main } from '../../src/index.js';
 import { resolve } from '../../src/util/path.js';
 import baseArguments from '../helpers/baseArguments.js';
-import baseCounters from '../helpers/baseCounters';
+import baseCounters from '../helpers/baseCounters.js';
 
 const cwd = resolve('tests/fixtures/plugins/tailwind');
 
