a172a86b282c5cdd195191e6b743183c7a7a8ab0
Lars Kappert <lars@webpro.nl>
2023-04-23 10:42:55 +0200

    Rename test title (all unique)

--- tests/paths-knip-config-workspaces.test.ts
+++ tests/paths-knip-config-workspaces.test.ts
@@ -5,7 +5,7 @@ import { resolve } from '../src/util/path.js';
 import baseArguments from './helpers/baseArguments.js';
 import baseCounters from './helpers/baseCounters.js';
 
-test('Resolve import path aliases', async () => {
+test('Resolve import path aliases (workspaces)', async () => {
   const cwd = resolve('tests/fixtures/paths-knip-config-workspaces');
 
   const { counters } = await main({
