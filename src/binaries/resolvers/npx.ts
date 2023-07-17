535b57378b11845cfa692a4278ec4c06242d8848
Lars Kappert <lars@webpro.nl>
2023-07-17 11:51:51 +0200

    Only explicit `--yes` will ignore dependency in npx resolver

--- src/binaries/resolvers/npx.ts
+++ src/binaries/resolvers/npx.ts
@@ -3,9 +3,14 @@ import { fromBinary } from '../util.js';
 import type { Resolver } from '../types.js';
 
 export const resolve: Resolver = (binary, args, { fromArgs }) => {
-  const parsed = parseArgs(args, { '--': true, stopEarly: true, boolean: ['yes', 'no'], alias: { yes: 'y', no: 'n' } });
+  const parsed = parseArgs(args, {
+    '--': true,
+    stopEarly: true,
+    boolean: ['yes', 'no'],
+    alias: { yes: 'y', no: 'no-install' },
+  });
   const leftParsed = fromArgs(parsed._);
-  const left = parsed.yes ? leftParsed.slice(1) : leftParsed;
+  const left = parsed.yes ? leftParsed.slice(1) : leftParsed; // Only explicit `npx --yes dep` indicates dep should not be in deps
   const packageName = left[0] ? [fromBinary(left[0])] : [];
   const right = parsed['--'] ? fromArgs(parsed['--']) : [];
   return [...packageName, ...left.slice(1), ...right];
