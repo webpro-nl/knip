871b4a9f10a5ec1c6dc186192980935ec9872718
Lars Kappert <lars@webpro.nl>
2023-08-22 15:04:15 +0200

    Move nestjs-middleware fixture/test files

---
+++ tests/plugins/next-middleware.test.ts
@@ -0,0 +1,21 @@
+import assert from 'node:assert/strict';
+import test from 'node:test';
+import { main } from '../../src/index.js';
+import { resolve } from '../../src/util/path.js';
+import baseArguments from '../helpers/baseArguments.js';
+import baseCounters from '../helpers/baseCounters.js';
+
+test('Support Next.js middleware', async () => {
+  const cwd = resolve('fixtures/plugins/next-middleware');
+
+  const { counters } = await main({
+    ...baseArguments,
+    cwd,
+  });
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    processed: 3,
+    total: 3,
+  });
+});
