79c46d8a20fbc812d50c3101570703691ff3c26d
Lars Kappert <lars@webpro.nl>
2023-09-06 21:19:54 +0200

    Fix up lefthook plugin (resolves #231)

--- src/plugins/lefthook/index.ts
+++ src/plugins/lefthook/index.ts
@@ -1,4 +1,7 @@
+import { readFileSync } from 'fs';
 import { _getDependenciesFromScripts } from '../../binaries/index.js';
+import { fromBinary } from '../../binaries/util.js';
+import { getGitHookPaths } from '../../util/git.js';
 import { getValuesByKeyDeep } from '../../util/object.js';
 import { timerify } from '../../util/Performance.js';
 import { hasDependency, load } from '../../util/plugin.js';
@@ -13,20 +16,23 @@ export const ENABLERS = ['lefthook', '@arkweid/lefthook'];
 
 export const isEnabled: IsPluginEnabledCallback = ({ dependencies }) => hasDependency(dependencies, ENABLERS);
 
-export const CONFIG_FILE_PATTERNS = ['lefthook.yml'];
+const gitHookPaths = getGitHookPaths();
+
+export const CONFIG_FILE_PATTERNS = ['lefthook.yml', ...gitHookPaths];
 
 const findLefthookDependencies: GenericPluginCallback = async (configFilePath, { cwd, manifest }) => {
-  const config = await load(configFilePath);
+  if (configFilePath.endsWith('.yml')) {
+    const config = await load(configFilePath);
+    if (!config) return [];
+    const scripts = getValuesByKeyDeep(config, 'run').filter((value): value is string => typeof value === 'string');
+    return _getDependenciesFromScripts(scripts, { cwd, manifest, knownGlobalsOnly: true });
+  }
 
-  if (!config) return [];
-
-  const scripts = getValuesByKeyDeep(config, 'run').filter((value): value is string => typeof value === 'string');
-
-  return _getDependenciesFromScripts(scripts, {
-    cwd,
-    manifest,
-    knownGlobalsOnly: true,
-  });
+  const script = readFileSync(configFilePath, 'utf8');
+  const scriptDependencies = _getDependenciesFromScripts([script], { cwd, manifest, knownGlobalsOnly: false });
+  const dependencies = manifest.devDependencies ? Object.keys(manifest.devDependencies) : [];
+  const matches = scriptDependencies.find(dep => dependencies.includes(fromBinary(dep)));
+  return matches ? [matches] : [];
 };
 
 export const findDependencies = timerify(findLefthookDependencies);
---
+++ tests/plugins/lefthook-v1.test.ts
@@ -0,0 +1,26 @@
+import assert from 'node:assert/strict';
+import fs from 'node:fs/promises';
+import test from 'node:test';
+import { main } from '../../src/index.js';
+import { join, resolve } from '../../src/util/path.js';
+import baseArguments from '../helpers/baseArguments.js';
+import baseCounters from '../helpers/baseCounters.js';
+
+const cwd = resolve('fixtures/plugins/lefthook-v1');
+
+test('Find dependencies in lefthook v1 configuration', async () => {
+  await fs.rename(join(cwd, '_git'), join(cwd, '.git')); // Can't add .git folder to repo
+
+  const { counters } = await main({
+    ...baseArguments,
+    cwd,
+  });
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    processed: 0,
+    total: 0,
+  });
+
+  await fs.rename(join(cwd, '.git'), join(cwd, '_git'));
+});
