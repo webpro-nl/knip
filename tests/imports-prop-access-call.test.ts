a51a77c98f1ae13f50ff6e7b1a877d7184cdb658
Lars Kappert <lars@webpro.nl>
2023-09-13 22:05:31 +0200

    Move requireResolve call to generic prop access visitor + tests

---
+++ tests/imports-prop-access-call.test.ts
@@ -0,0 +1,21 @@
+import assert from 'node:assert/strict';
+import test from 'node:test';
+import { main } from '../src/index.js';
+import { resolve } from '../src/util/path.js';
+import baseArguments from './helpers/baseArguments.js';
+import baseCounters from './helpers/baseCounters.js';
+
+const cwd = resolve('fixtures/imports-prop-access-call');
+
+test('Support various prop access calls', async () => {
+  const { counters } = await main({
+    ...baseArguments,
+    cwd,
+  });
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    processed: 2,
+    total: 2,
+  });
+});
