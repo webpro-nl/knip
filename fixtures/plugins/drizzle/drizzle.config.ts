c310162c095d6c84105b9f7dd23b865e7c5d4ce3
Julien Goux <hi@jgoux.dev>
2023-09-25 21:30:28 +0200

    drizzle plugin (#266)
    * drizzle plugin
    
    * support schemas
    
    * use _glob

---
+++ fixtures/plugins/drizzle/drizzle.config.ts
@@ -0,0 +1,5 @@
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: ["./src/schema.ts", "./src/schema/*.ts"],
+} satisfies Config;
---
+++ tests/plugins/drizzle.test.ts
@@ -0,0 +1,23 @@
+import assert from 'node:assert/strict';
+import test from 'node:test';
+import { main } from '../../src/index.js';
+import { resolve } from '../../src/util/path.js';
+import baseArguments from '../helpers/baseArguments.js';
+import baseCounters from '../helpers/baseCounters.js';
+
+const cwd = resolve('fixtures/plugins/drizzle');
+
+test('Find dependencies in Drizzle configuration', async () => {
+  const { counters } = await main({
+    ...baseArguments,
+    cwd,
+  });
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    devDependencies: 0,
+    unlisted: 0,
+    processed: 3,
+    total: 3,
+  });
+});
