e3916f629daf0348af46028f014734e28e4b2733
jgoux <hi@jgoux.dev>
2023-09-26 07:21:24 +0200

    support vitest config within workspace

--- fixtures/plugins/vitest/vitest.workspace.ts
+++ fixtures/plugins/vitest/vitest.workspace.ts
@@ -1,3 +1,13 @@
 import { defineWorkspace } from "vitest/config";
 
-export default defineWorkspace(["apps/*", "packages/*"]);
+export default defineWorkspace([
+  "apps/*",
+  "packages/*",
+  {
+    test: {
+      include: ['tests/**/*.{edge}.test.{ts,js}'],
+      name: 'edge',
+      environment: 'edge-runtime',
+    }
+  },
+]);
