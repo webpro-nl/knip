c45a337440521f5396fdc56f91009b100ce044a2
Lars Kappert <lars@webpro.nl>
2023-09-28 09:13:08 +0200

    Add types + support for projects in playwright plugin

--- src/plugins/playwright/index.ts
+++ src/plugins/playwright/index.ts
@@ -1,8 +1,10 @@
-import { dirname, join } from '../../util/path.js';
+import { compact } from '../../util/array.js';
+import { dirname, join, relative } from '../../util/path.js';
 import { timerify } from '../../util/Performance.js';
 import { hasDependency, load } from '../../util/plugin.js';
 import { toEntryPattern } from '../../util/protocols.js';
 import type { GenericPluginCallback, IsPluginEnabledCallback } from '../../types/plugins.js';
+import type { PlaywrightTestConfig } from 'playwright/test';
 
 // https://playwright.dev/docs/test-configuration
 
@@ -18,11 +20,18 @@ export const ENTRY_FILE_PATTERNS = ['**/*.@(spec|test).?(c|m)[jt]s?(x)'];
 
 export const CONFIG_FILE_PATTERNS = ['playwright.config.{js,ts}'];
 
-const findPlaywrightDependencies: GenericPluginCallback = async configFilePath => {
-  const cfg = await load(configFilePath);
-  const dir = cfg.testDir ? join(dirname(configFilePath), cfg.testDir) : dirname(configFilePath);
-  const entryPatterns = (cfg.testMatch ? [cfg.testMatch] : ENTRY_FILE_PATTERNS).map(p => toEntryPattern(join(dir, p)));
+const findProjectDependencies = (cwd: string, configFilePath: string, config: PlaywrightTestConfig) => {
+  const dir = relative(cwd, config.testDir ? join(dirname(configFilePath), config.testDir) : dirname(configFilePath));
+  const entryPatterns = (config.testMatch ? [config.testMatch] : ENTRY_FILE_PATTERNS).flatMap(pattern =>
+    [pattern].flat().flatMap(pattern => (typeof pattern === 'string' ? toEntryPattern(join(dir, pattern)) : []))
+  );
   return entryPatterns;
+};
+
+const findPlaywrightDependencies: GenericPluginCallback = async (configFilePath, { cwd }) => {
+  const config: PlaywrightTestConfig = await load(configFilePath);
+  const projects = config.projects ? config.projects : [config];
+  return compact(projects.flatMap(project => findProjectDependencies(cwd, configFilePath, project)));
 };
 
 export const findDependencies = timerify(findPlaywrightDependencies);
