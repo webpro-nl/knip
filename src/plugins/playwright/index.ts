9ce013f4f98a532f0db9c4d71a452b5d9022657c
Lars Kappert <lars@webpro.nl>
2023-10-07 10:58:48 +0200

    Fix up playwright plugin

--- src/plugins/playwright/index.ts
+++ src/plugins/playwright/index.ts
@@ -1,4 +1,3 @@
-import { compact } from '../../util/array.js';
 import { dirname, join, relative } from '../../util/path.js';
 import { timerify } from '../../util/Performance.js';
 import { hasDependency, load } from '../../util/plugin.js';
@@ -20,18 +19,24 @@ export const ENTRY_FILE_PATTERNS = ['**/*.@(spec|test).?(c|m)[jt]s?(x)'];
 
 export const CONFIG_FILE_PATTERNS = ['playwright.config.{js,ts}'];
 
-const findProjectDependencies = (cwd: string, configFilePath: string, config: PlaywrightTestConfig) => {
+const toEntryPatterns = (
+  testMatch: string | RegExp | Array<string | RegExp> | undefined,
+  cwd: string,
+  configFilePath: string,
+  config: PlaywrightTestConfig
+) => {
+  if (!testMatch) return [];
   const dir = relative(cwd, config.testDir ? join(dirname(configFilePath), config.testDir) : dirname(configFilePath));
-  const entryPatterns = (config.testMatch ? [config.testMatch] : ENTRY_FILE_PATTERNS).flatMap(pattern =>
-    [pattern].flat().flatMap(pattern => (typeof pattern === 'string' ? toEntryPattern(join(dir, pattern)) : []))
-  );
-  return entryPatterns;
+  const patterns = [testMatch].flat().filter((p): p is string => typeof p === 'string');
+  return patterns.map(pattern => toEntryPattern(join(dir, pattern)));
 };
 
 const findPlaywrightDependencies: GenericPluginCallback = async (configFilePath, { cwd }) => {
   const config: PlaywrightTestConfig = await load(configFilePath);
-  const projects = config.projects ? config.projects : [config];
-  return compact(projects.flatMap(project => findProjectDependencies(cwd, configFilePath, project)));
+  const projects = config.projects ? [config, ...config.projects] : [config];
+  const patterns = projects.flatMap(config => toEntryPatterns(config.testMatch, cwd, configFilePath, config));
+  if (patterns.length > 0) return patterns;
+  return toEntryPatterns(ENTRY_FILE_PATTERNS, cwd, configFilePath, config);
 };
 
 export const findDependencies = timerify(findPlaywrightDependencies);
