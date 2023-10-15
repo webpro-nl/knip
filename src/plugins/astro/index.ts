92b72a5750b81d311baa2c770d760c8690a175cb
Dániel Földi <dani@stefan.hu>
2023-10-15 20:56:16 +0200

    Narrow entry file pattern

--- src/plugins/astro/index.ts
+++ src/plugins/astro/index.ts
@@ -12,6 +12,6 @@ export const isEnabled: IsPluginEnabledCallback = ({ dependencies }) => hasDepen
 
 export const ENTRY_FILE_PATTERNS = ['astro.config.{js,cjs,mjs,ts}', 'src/content/config.ts'];
 
-export const PRODUCTION_ENTRY_FILE_PATTERNS = ['src/pages/**/*'];
+export const PRODUCTION_ENTRY_FILE_PATTERNS = ['src/pages/**/*.{astro,md,mdx,html,js,ts}'];
 
 export const PROJECT_FILE_PATTERNS = ['src/**/*'];
