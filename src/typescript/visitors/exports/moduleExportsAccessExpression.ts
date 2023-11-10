4fb6239cfffa27ae2a1e16244424a8575ad15d3b
Lars Kappert <lars@webpro.nl>
2023-11-10 10:49:34 +0100

    Increase readability of `module.exports` visitor

--- src/typescript/visitors/exports/moduleExportsAccessExpression.ts
+++ src/typescript/visitors/exports/moduleExportsAccessExpression.ts
@@ -4,48 +4,43 @@ import { stripQuotes } from '../../ast-helpers.js';
 import { isJS } from '../helpers.js';
 import { exportVisitor as visit } from '../index.js';
 
+const isModuleExportsAccess = (node: ts.PropertyAccessExpression) =>
+  ts.isIdentifier(node.expression) && node.expression.escapedText === 'module' && node.name.escapedText === 'exports';
+
 export default visit(isJS, node => {
   if (ts.isExpressionStatement(node)) {
     if (ts.isBinaryExpression(node.expression)) {
-      if (
-        ts.isPropertyAccessExpression(node.expression.left) &&
-        ts.isPropertyAccessExpression(node.expression.left.expression) &&
-        ts.isIdentifier(node.expression.left.expression.expression) &&
-        node.expression.left.expression.expression.escapedText === 'module' &&
-        node.expression.left.expression.name.escapedText === 'exports'
-      ) {
-        // Pattern: module.exports.NAME
-        const identifier = node.expression.left.name.getText();
-        const pos = node.expression.left.name.pos;
-        return { node, identifier, type: SymbolType.UNKNOWN, pos };
+      if (ts.isPropertyAccessExpression(node.expression.left)) {
+        if (
+          ts.isPropertyAccessExpression(node.expression.left.expression) &&
+          isModuleExportsAccess(node.expression.left.expression)
+        ) {
+          // Pattern: module.exports.NAME
+          const identifier = node.expression.left.name.getText();
+          const pos = node.expression.left.name.pos;
+          return { node, identifier, type: SymbolType.UNKNOWN, pos };
+        } else if (isModuleExportsAccess(node.expression.left)) {
+          const expr = node.expression.right;
+          if (ts.isObjectLiteralExpression(expr) && expr.properties.every(ts.isShorthandPropertyAssignment)) {
+            // Pattern: module.exports = { identifier, identifier2 }
+            return expr.properties.map(node => {
+              return { node, identifier: node.getText(), type: SymbolType.UNKNOWN, pos: node.pos };
+            });
+          } else {
+            // Pattern: module.exports = any
+            return { node, identifier: 'default', type: SymbolType.UNKNOWN, pos: expr.pos };
+          }
+        }
       } else if (
         ts.isElementAccessExpression(node.expression.left) &&
         ts.isPropertyAccessExpression(node.expression.left.expression) &&
         ts.isIdentifier(node.expression.left.expression.name) &&
-        ts.isIdentifier(node.expression.left.expression.expression) &&
-        node.expression.left.expression.expression.escapedText === 'module' &&
-        node.expression.left.expression.name.escapedText === 'exports'
+        isModuleExportsAccess(node.expression.left.expression)
       ) {
         // Pattern: module.exports['NAME']
         const identifier = stripQuotes(node.expression.left.argumentExpression.getText());
         const pos = node.expression.left.argumentExpression.pos;
         return { node, identifier, type: SymbolType.UNKNOWN, pos };
-      } else if (
-        ts.isPropertyAccessExpression(node.expression.left) &&
-        ts.isIdentifier(node.expression.left.expression) &&
-        node.expression.left.expression.escapedText === 'module' &&
-        node.expression.left.name.escapedText === 'exports'
-      ) {
-        const expr = node.expression.right;
-        if (ts.isObjectLiteralExpression(expr) && expr.properties.every(ts.isShorthandPropertyAssignment)) {
-          // Pattern: module.exports = { identifier, identifier2 }
-          return expr.properties.map(node => {
-            return { node, identifier: node.getText(), type: SymbolType.UNKNOWN, pos: node.pos };
-          });
-        } else {
-          // Pattern: module.exports = any
-          return { node, identifier: 'default', type: SymbolType.UNKNOWN, pos: expr.pos };
-        }
       }
     }
   }
