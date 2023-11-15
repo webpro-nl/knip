234672e5d825315bc1660c6b44fa234bca72c28a
Lars Kappert <lars@webpro.nl>
2023-11-15 17:53:20 +0100

    Fix astro plugin: ignore `@astrojs/check` in production mode

--- packages/knip/src/plugins/astro/index.ts
+++ packages/knip/src/plugins/astro/index.ts
@@ -18,13 +18,17 @@ export const ENTRY_FILE_PATTERNS = ['astro.config.{js,cjs,mjs,ts}', 'src/content
 export const PRODUCTION_ENTRY_FILE_PATTERNS = ['src/pages/**/*.{astro,mdx,js,ts}', 'src/content/**/*.mdx'];
 
 export const findDependencies: GenericPluginCallback = async (configFilePath, options) => {
-  const { config, manifest } = options;
+  const { config, manifest, isProduction } = options;
 
   const dependencies = config.entry
     ? config.entry.map(toProductionEntryPattern)
     : [...ENTRY_FILE_PATTERNS.map(toEntryPattern), ...PRODUCTION_ENTRY_FILE_PATTERNS.map(toProductionEntryPattern)];
 
-  if (manifest.scripts && Object.values(manifest.scripts).some(script => /astro (--.+ )?check/.test(script))) {
+  if (
+    !isProduction &&
+    manifest.scripts &&
+    Object.values(manifest.scripts).some(script => /astro (--.+ )?check/.test(script))
+  ) {
     dependencies.push('@astrojs/check');
   }
 
