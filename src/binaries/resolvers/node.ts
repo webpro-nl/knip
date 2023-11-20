c0a9c5bc76184a41ba7763943be2113d5db891fb
Lars Kappert <lars@webpro.nl>
2023-11-20 09:00:19 +0100

    Add --import argument to node resolver (resolves #351)

--- src/binaries/resolvers/node.ts
+++ src/binaries/resolvers/node.ts
@@ -6,7 +6,7 @@ import type { Resolver } from '../types.js';
 export const resolve: Resolver = (binary, args, { cwd }) => {
   const parsed = parseArgs(args, {
     string: ['r'],
-    alias: { require: ['r', 'loader', 'experimental-loader', 'test-reporter', 'watch'] },
+    alias: { require: ['r', 'loader', 'experimental-loader', 'test-reporter', 'watch', 'import'] },
   });
   return compact([tryResolveFilePath(cwd, parsed._[0]), ...tryResolveSpecifiers(cwd, [parsed.require].flat())]);
 };
--- test/util/getReferencesFromScripts.test.ts
+++ test/util/getReferencesFromScripts.test.ts
@@ -37,6 +37,7 @@ test('getReferencesFromScripts (node)', () => {
   t('node -r package/script', ['package']);
   t('node -r ./require.js script.js', [js, req]);
   t('node --require=pkg1 --require pkg2 script', [js, 'pkg1', 'pkg2']);
+  t('node --import tsx ./main.ts', [ts, 'tsx']);
   t('node --loader ts-node/esm node_modules/webpack-cli/bin/cli.js -c ./webpack.config.ts', ['webpack-cli', 'ts-node']);
   t('node --experimental-loader ts-node/esm/transpile-only script.js', [js, 'ts-node']);
   // TODO With jiti's `esmResolve: true` dirs are no longer resolved to their index.js (but extensionless is OK)
