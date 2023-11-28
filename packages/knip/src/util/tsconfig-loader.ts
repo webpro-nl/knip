7c80ad163ecabffe8127e889120f874b63cbe94c
Lars Kappert <lars@webpro.nl>
2023-11-28 08:51:11 +0100

    Don't try to load fake path in tsconfig-loader

--- packages/knip/src/util/tsconfig-loader.ts
+++ packages/knip/src/util/tsconfig-loader.ts
@@ -1,9 +1,10 @@
 import ts from 'typescript';
 import { isFile } from './fs.js';
+import { FAKE_PATH } from './loader.js';
 import { dirname } from './path.js';
 
 export const loadTSConfig = async (tsConfigFilePath: string) => {
-  if (isFile(tsConfigFilePath)) {
+  if (tsConfigFilePath !== FAKE_PATH && isFile(tsConfigFilePath)) {
     const config = ts.readConfigFile(tsConfigFilePath, ts.sys.readFile);
     const parsedConfig = ts.parseJsonConfigFileContent(config.config, ts.sys, dirname(tsConfigFilePath));
     const compilerOptions = parsedConfig.options ?? {};
