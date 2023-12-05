1f784b48e3697f086de4ca246489d7d10cc92eb6
Lars Kappert <lars@webpro.nl>
2023-12-05 13:10:03 +0100

    Improve heuristic for enabling node-test-runner plugin

--- packages/knip/src/plugins/node-test-runner/index.ts
+++ packages/knip/src/plugins/node-test-runner/index.ts
@@ -9,12 +9,11 @@ export const NAME = 'Node.js Test Runner';
 /** @public */
 export const ENABLERS = 'This plugin is enabled when any script in `package.json` includes `node --test`';
 
-export const isEnabled: IsPluginEnabledCallback = ({ manifest }) => {
-  // TODO Better to scan the entry files until the first `node:test` import, but that's expensive
-  return Object.keys(manifest.scripts ?? {})
-    .filter(s => /test/.test(s)) // TODO Checking all script may return false positives to match `node --test`?
-    .some(s => manifest.scripts && /node (.*)--test/.test(manifest.scripts[s]));
-};
+// TODO Better to scan the entry files until the first `node:test` import, but that's expensive
+export const isEnabled: IsPluginEnabledCallback = ({ manifest }) =>
+  Object.keys(manifest.scripts ?? {}).some(
+    script => manifest.scripts && /(?<=^|\s)node (.*)--test/.test(manifest.scripts[script])
+  );
 
 /** @public */
 export const ENTRY_FILE_PATTERNS = [
