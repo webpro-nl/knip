aeb91376aaa26dddc1d7fdbaec1c671f968d6b2c
Lars Kappert <lars@webpro.nl>
2023-12-05 17:01:33 +0100

    Fix sitemap

--- packages/docs/src/pages/sitemap.txt.ts
+++ packages/docs/src/pages/sitemap.txt.ts
@@ -1,11 +1,16 @@
 import type { APIContext } from 'astro';
 
 const data = await import.meta.glob(['/src/content/**/!([)*.{md,mdx}'], { eager: true });
+const plugins = await import.meta.glob(['/dist/reference/plugins/*/index.html'], { eager: true });
 
 const pages = new Set<string>();
 for (const [filePath, page] of Object.entries(data)) {
   if (page?.frontmatter?.draft === true) continue;
-  pages.add(filePath.replace(/^\/src\/content\//, '').replace(/(\/?index)?\.(md|md)$/, ''));
+  pages.add(filePath.replace(/^\/src\/content\/docs\//, '').replace(/(\/?index)?\.(md|mdx)$/, ''));
+}
+
+for (const [filePath] of Object.entries(plugins)) {
+  pages.add(filePath.replace(/^\/dist\//, '').replace(/\/index.html$/, ''));
 }
 
 export async function GET(context: APIContext) {
