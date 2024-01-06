d9db45b94713dd702d8467a29694debfc3397fb2
auvred <aauvred@gmail.com>
2024-01-06 08:05:52 +0000

    Add support for JavaScript GitHub Actions

--- packages/knip/src/plugins/github-actions/index.ts
+++ packages/knip/src/plugins/github-actions/index.ts
@@ -1,8 +1,13 @@
 import { _firstGlob } from '../../util/glob.js';
 import { getValuesByKeyDeep } from '../../util/object.js';
+import { dirname, join } from '../../util/path.js';
 import { timerify } from '../../util/Performance.js';
 import { getDependenciesFromScripts, load } from '../../util/plugin.js';
 import type { IsPluginEnabledCallback, GenericPluginCallback } from '../../types/plugins.js';
+
+function isString(value: unknown): value is string {
+  return typeof value === 'string';
+}
 
 // https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions
 
@@ -26,13 +31,27 @@ const findGithubActionsDependencies: GenericPluginCallback = async (configFilePa
 
   if (!config) return [];
 
-  const scripts = getValuesByKeyDeep(config, 'run').filter((value): value is string => typeof value === 'string');
+  const scripts = getValuesByKeyDeep(config, 'run').filter(isString);
 
-  return getDependenciesFromScripts(scripts, {
-    cwd,
-    manifest,
-    knownGlobalsOnly: true,
-  });
+  return [
+    ...getActionDependencies(),
+    ...getDependenciesFromScripts(scripts, {
+      cwd,
+      manifest,
+      knownGlobalsOnly: true,
+    }),
+  ];
+
+  function getActionDependencies() {
+    const isActionManifest = configFilePath.endsWith('action.yml') || configFilePath.endsWith('action.yaml');
+    if (!isActionManifest || !config?.runs?.using?.startsWith('node')) {
+      return [];
+    }
+
+    const scripts = [config.runs.pre, config.runs.main, config.runs.post].filter(isString);
+
+    return scripts.map(script => join(dirname(configFilePath), script));
+  }
 };
 
 export const findDependencies = timerify(findGithubActionsDependencies);
--- packages/knip/test/plugins/github-actions.test.ts
+++ packages/knip/test/plugins/github-actions.test.ts
@@ -28,3 +28,41 @@ test('Find dependencies in github-actions workflow configurations', async () => 
     join(cwd, 'scripts/get-release-notes.js'),
   ]);
 });
+
+test('Find dependencies in github-actions composite action', async () => {
+  const configFilePath = join(cwd, '.github/actions/composite/action.yml');
+  const dependencies = await GithubActions.findDependencies(configFilePath, { cwd, manifest });
+  assert.deepEqual(dependencies, [
+    join(cwd, 'comment.ts'),
+    'esbuild-register',
+    join(cwd, 'scripts/check-dependencies.js'),
+    'bin:playwright',
+    'bin:prisma',
+    'bin:eslint',
+    'bin:release-it',
+    'bin:knip',
+    'bin:nyc',
+    'retry-cli',
+    'bin:curl',
+    '@scope/retry-cli',
+    'bin:changeset',
+    'bin:wait-on',
+    join(cwd, 'scripts/get-release-notes.js'),
+  ]);
+});
+
+test('Find dependencies in github-actions node action A', async () => {
+  const configFilePath = join(cwd, '.github/actions/node-a/action.yml');
+  const dependencies = await GithubActions.findDependencies(configFilePath, { cwd, manifest });
+  assert.deepEqual(dependencies, [
+    join(cwd, '.github/actions/node-a/dist/pre.js'),
+    join(cwd, '.github/actions/node-a/main.js'),
+    join(cwd, '.github/actions/node-a/post.js'),
+  ]);
+});
+
+test('Find dependencies in github-actions node action B', async () => {
+  const configFilePath = join(cwd, '.github/actions/node-b/action.yaml');
+  const dependencies = await GithubActions.findDependencies(configFilePath, { cwd, manifest });
+  assert.deepEqual(dependencies, [join(cwd, '.github/actions/node-b/main.js')]);
+});
