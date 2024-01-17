8d6eba75094f4e5da06c597560d9cea49d11a3f5
uncenter <47499684+uncenter@users.noreply.github.com>
2024-01-17 09:40:38 -0500

    Remove unneeded templateFormats

--- packages/knip/src/plugins/eleventy/helpers.ts
+++ packages/knip/src/plugins/eleventy/helpers.ts
@@ -149,5 +149,5 @@ export const defaultEleventyConfig: EleventyConfig = {
     layouts: '_includes',
     data: '_data',
   },
-  templateFormats: 'html,liquid,ejs,md,hbs,mustache,haml,pug,njk,11ty.js',
+  templateFormats: '11ty.js',
 };
