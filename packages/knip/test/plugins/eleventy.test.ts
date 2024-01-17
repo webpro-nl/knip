e1077292f410f0d1becd6a4a37dad80b8b039fcd
uncenter <47499684+uncenter@users.noreply.github.com>
2024-01-17 10:01:57 -0500

    Add new dynamic test and fixture, update old one to be static

--- packages/knip/test/plugins/eleventy.test.ts
+++ packages/knip/test/plugins/eleventy.test.ts
@@ -7,12 +7,18 @@ import { getManifest, pluginConfig as config } from '../helpers/index.js';
 const cwd = resolve('fixtures/plugins/eleventy');
 const manifest = getManifest(cwd);
 
-test('Find dependencies in Eleventy configuration', async () => {
+test('Find dependencies in Eleventy configuration (static)', async () => {
   const configFilePath = join(cwd, 'eleventy.config.cjs');
+  const dependencies = await eleventy.findDependencies(configFilePath, { manifest, config });
+  assert.deepEqual(dependencies, ['entry:_data/**/*.js', 'entry:**/*.{11ty.js}', 'entry:**/*.11tydata.js']);
+});
+
+test('Find dependencies in Eleventy configuration (dynamic)', async () => {
+  const configFilePath = join(cwd, 'eleventy.config.dynamic.cjs');
   const dependencies = await eleventy.findDependencies(configFilePath, { manifest, config });
   assert.deepEqual(dependencies, [
     'entry:_data/**/*.js',
-    'entry:**/*.{html,liquid,ejs,md,hbs,mustache,haml,pug,njk,11ty.js}',
-    'entry:**/*.11tydata.js',
+    'entry:content/**/*.{md,njk,html,liquid}',
+    'entry:content/**/*.11tydata.js',
   ]);
 });
