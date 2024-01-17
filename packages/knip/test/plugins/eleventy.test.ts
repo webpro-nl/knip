92470075be332472d9f3aaef0b1f8016f072a7a4
uncenter <47499684+uncenter@users.noreply.github.com>
2024-01-17 07:54:35 -0500

    Update test

--- packages/knip/test/plugins/eleventy.test.ts
+++ packages/knip/test/plugins/eleventy.test.ts
@@ -1,21 +1,18 @@
 import assert from 'node:assert/strict';
 import test from 'node:test';
-import { main } from '../../src/index.js';
-import { resolve } from '../../src/util/path.js';
-import baseArguments from '../helpers/baseArguments.js';
-import baseCounters from '../helpers/baseCounters.js';
+import { default as eleventy } from '../../src/plugins/eleventy/index.js';
+import { resolve, join } from '../../src/util/path.js';
+import { getManifest, pluginConfig as config } from '../helpers/index.js';
 
 const cwd = resolve('fixtures/plugins/eleventy');
+const manifest = getManifest(cwd);
 
 test('Find dependencies in Eleventy configuration', async () => {
-  const { counters } = await main({
-    ...baseArguments,
-    cwd,
-  });
-
-  assert.deepEqual(counters, {
-    ...baseCounters,
-    processed: 2,
-    total: 2,
-  });
+  const configFilePath = join(cwd, 'eleventy.config.cjs');
+  const dependencies = await eleventy.findDependencies(configFilePath, { manifest, config });
+  assert.deepEqual(dependencies, [
+    'entry:_data/**/*.js',
+    'entry:**/*.{html,liquid,ejs,md,hbs,mustache,haml,pug,njk,11ty.js}',
+    'entry:**/*.11tydata.js',
+  ]);
 });
