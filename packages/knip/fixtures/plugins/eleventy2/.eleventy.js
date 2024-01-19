dbf8c02cc5696f8dbe54bb3a20691c6954975cda
Lars Kappert <lars@webpro.nl>
2024-01-19 09:03:44 +0100

    Add additional test & fixture for eleventy plugin

---
+++ packages/knip/fixtures/plugins/eleventy2/.eleventy.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const path = require('path');
+const highlighter = require('./src/_plugins/syntax-highlighter');
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addGlobalData('site_name', 'example');
+  eleventyConfig.addDataExtension('yml', () => {});
+  eleventyConfig.addFilter('limitTo', () => {});
+  eleventyConfig.addFilter('sortByPageOrder', () => {});
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    alwaysWrapLineHighlights: true,
+    templateFormats: ['liquid', 'njk'],
+  });
+  eleventyConfig.setLibrary('md', () => {});
+  eleventyConfig.addNunjucksShortcode('link', () => {});
+  eleventyConfig.addShortcode('related_rules', () => {});
+  eleventyConfig.addWatchTarget('./src/assets/');
+  eleventyConfig.addPassthroughCopy({
+    './src/static': '/',
+  });
+  eleventyConfig.addPassthroughCopy('./src/assets/');
+  eleventyConfig.addPassthroughCopy('src/static');
+  eleventyConfig.addCollection('docs', () => {});
+  eleventyConfig.addCollection('library', () => {});
+  eleventyConfig.ignores.add('src/static/sitemap.njk');
+
+  eleventyConfig.addPassthroughCopy({
+    './node_modules/@org/lib/dist/build.js': '/assets/lib.js',
+  });
+
+  return {
+    passthroughFileCopy: true,
+    pathPrefix: '/docs/head/',
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      layouts: '_includes/layouts',
+      data: '_data',
+      output: '_site',
+    },
+  };
+};
---
+++ packages/knip/test/plugins/eleventy2.test.ts
@@ -0,0 +1,21 @@
+import assert from 'node:assert/strict';
+import test from 'node:test';
+import { main } from '../../src/index.js';
+import { resolve } from '../../src/util/path.js';
+import baseArguments from '../helpers/baseArguments.js';
+import baseCounters from '../helpers/baseCounters.js';
+
+const cwd = resolve('fixtures/plugins/eleventy2');
+
+test('Find dependencies in Eleventy configuration (static)', async () => {
+  const { counters } = await main({
+    ...baseArguments,
+    cwd,
+  });
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    processed: 4,
+    total: 4,
+  });
+});
