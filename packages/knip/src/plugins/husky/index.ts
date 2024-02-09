1edc9fbfccaa03c77f25ae88d9bc3f724aa908ff
Risu <79110363+risu729@users.noreply.github.com>
2024-02-10 06:38:11 +0900

    Support husky v9 (#500)
    * Support husky v9
    
    * Keep the behavior for husky v8 or lower
    
    * Split test for husky into v8 and v9

--- packages/knip/src/plugins/husky/index.ts
+++ packages/knip/src/plugins/husky/index.ts
@@ -1,3 +1,4 @@
+import semver from 'semver';
 import { getGitHookPaths } from '../../util/git.js';
 import { timerify } from '../../util/Performance.js';
 import { getDependenciesFromScripts, hasDependency, loadFile } from '../../util/plugin.js';
@@ -11,14 +12,34 @@ const ENABLERS = ['husky'];
 
 const isEnabled: IsPluginEnabledCallback = ({ dependencies }) => hasDependency(dependencies, ENABLERS);
 
-const gitHookPaths = getGitHookPaths('.husky');
+const gitHooksPathInV8 = getGitHookPaths('.husky', true);
+// husky v9 registers hooks in .husky/_/ to git and calls user defined hooks in .husky/ from there
+const gitHookPathsInV9 = getGitHookPaths('.husky', false);
 
-const CONFIG_FILE_PATTERNS = [...gitHookPaths];
+// Add patterns for both v8 and v9 because we can't know which version is installed at this point
+const CONFIG_FILE_PATTERNS = [...gitHooksPathInV8, ...gitHookPathsInV9];
 
 const findHuskyDependencies: GenericPluginCallback = async (configFilePath, options) => {
-  const { isProduction } = options;
+  const { isProduction, manifest } = options;
 
   if (isProduction) return [];
+
+  const huskyVersion = manifest.devDependencies?.husky ?? manifest.dependencies?.husky ?? '*';
+
+  // Ignore config files that are not used by the installed husky version
+  const isV8OrLower = semver.intersects(huskyVersion, '<9', {
+    includePrerelease: true,
+  });
+  if (!isV8OrLower && gitHooksPathInV8.some(path => configFilePath.includes(path))) {
+    return [];
+  }
+
+  const isV9OrHigher = semver.intersects(huskyVersion, '>=9', {
+    includePrerelease: true,
+  });
+  if (!isV9OrHigher && gitHookPathsInV9.some(path => configFilePath.includes(path))) {
+    return [];
+  }
 
   const script = await loadFile(configFilePath);
 
