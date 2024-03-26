29eec2fc1ed7559cd895cee2c39c2b7bd66a5bb1
Lars Kappert <lars@webpro.nl>
2024-03-26 07:59:46 +0100

    Wrap up

--- packages/knip/src/plugins/wrangler/index.ts
+++ packages/knip/src/plugins/wrangler/index.ts
@@ -1,17 +1,16 @@
 import { hasDependency } from '#p/util/plugin.js';
-import type { IgnorePatterns } from '#p/types/config.js';
 import type { IsPluginEnabled, Plugin, ResolveEntryPaths } from '#p/types/plugins.js';
 import type { WranglerConfig } from './types.js';
 
 // https://developers.cloudflare.com/workers/wrangler/configuration/
 
-const title = 'wrangler';
+const title = 'Wrangler';
 
-const enablers: IgnorePatterns = ['wrangler'];
+const enablers = ['wrangler'];
 
 const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependencies, enablers);
 
-const config: string[] = ['wrangler.{json,toml}'];
+const config = ['wrangler.{json,toml}'];
 
 const resolveEntryPaths: ResolveEntryPaths<WranglerConfig> = async config => {
   return config.main ? [config.main] : [];
