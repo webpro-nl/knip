5cb4f5115cf53cad1248e034756b5869f4d54a01
Lars Kappert <lars@webpro.nl>
2024-04-19 10:39:36 +0200

    Remove obsolete workaround in tag helper

--- packages/knip/src/util/tag.ts
+++ packages/knip/src/util/tag.ts
@@ -15,8 +15,6 @@ export const splitTags = (rawTags: string[]) => {
 const hasTag = (tags: string[], jsDocTags: Set<string>) => tags.some(tag => jsDocTags.has(`@${tag}`));
 
 export const shouldIgnore = (jsDocTags: Set<string>, tags: Tags) => {
-  // TODO This should not be necessary (related to de/serialization):
-  if (Array.isArray(jsDocTags)) jsDocTags = new Set(jsDocTags);
   const [includeJSDocTags, excludeJSDocTags] = tags;
   if (includeJSDocTags.length > 0 && !hasTag(includeJSDocTags, jsDocTags)) return true;
   if (excludeJSDocTags.length > 0 && hasTag(excludeJSDocTags, jsDocTags)) return true;
