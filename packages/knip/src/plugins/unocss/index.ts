9826694fca7bc2989676aaa9533c42602616e7a4
Stephen Zhou <hi@hyoban.cc>
2024-05-16 11:02:09 +0800

    remove useless code

--- packages/knip/src/plugins/unocss/index.ts
+++ packages/knip/src/plugins/unocss/index.ts
@@ -1,7 +1,6 @@
 import type { EnablerPatterns } from '#p/types/config.js';
-import type { IsPluginEnabled, Plugin, ResolveConfig } from '#p/types/plugins.js';
+import type { IsPluginEnabled, Plugin } from '#p/types/plugins.js';
 import { hasDependency } from '#p/util/plugin.js';
-import type { PluginConfig } from './types.js';
 
 // https://unocss.dev/guide/config-file
 
@@ -13,21 +12,9 @@ const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependenc
 
 const config: string[] = ['uno.config.{js,ts,mjs,mts}', 'unocss.config.{js,ts,mjs,mts}'];
 
-const entry: string[] = [];
-
-const production: string[] = [];
-
-const resolveConfig: ResolveConfig<PluginConfig> = async config => {
-  const dependencies = config?.plugins ?? [];
-  return [...dependencies];
-};
-
 export default {
   title,
   enablers,
   isEnabled,
   config,
-  entry,
-  production,
-  resolveConfig,
 } satisfies Plugin;
