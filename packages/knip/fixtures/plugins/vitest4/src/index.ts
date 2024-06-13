9731d91ce6d89d1b7c19c836b01b60cf2f98fda8
Nebez Briefkani <me@nebezb.com>
2024-06-13 17:46:22 -0400

    Add unused import

--- packages/knip/fixtures/plugins/vitest4/src/index.ts
+++
@@ -1,1 +0,0 @@
-export const adder = (a: number, b: number) => a + b;
--- packages/knip/test/plugins/vitest4.test.ts
+++ packages/knip/test/plugins/vitest4.test.ts
@@ -1,21 +1,23 @@
 import { test } from 'bun:test';
 import assert from 'node:assert/strict';
 import { main } from '../../src/index.js';
-import { resolve } from '../../src/util/path.js';
+import { join, resolve } from '../../src/util/path.js';
 import baseArguments from '../helpers/baseArguments.js';
 import baseCounters from '../helpers/baseCounters.js';
 
 const cwd = resolve('fixtures/plugins/vitest4');
 
 test('Find dependencies with Vitest plugin (4)', async () => {
-  const { counters } = await main({
+  const { issues, counters } = await main({
     ...baseArguments,
     cwd,
   });
 
+  assert(issues.files.has(join(cwd, 'src/unused.test.ts')));
+
   assert.deepEqual(counters, {
     ...baseCounters,
-    files: 0,
+    files: 1,
     processed: 4,
     total: 4,
   });
