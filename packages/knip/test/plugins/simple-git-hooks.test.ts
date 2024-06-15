6737abaf281ae0e4a43eb330fad9a38ef80c5242
Stephen Zhou <hi@hyoban.cc>
2024-06-15 15:59:27 +0800

    assert

--- packages/knip/test/plugins/simple-git-hooks.test.ts
+++ packages/knip/test/plugins/simple-git-hooks.test.ts
@@ -8,12 +8,15 @@ import baseCounters from '../helpers/baseCounters.js';
 const cwd = resolve('fixtures/plugins/simple-git-hooks');
 
 test('Find dependencies with the simple-git-hooks plugin', async () => {
-  const { /* issues, */ counters } = await main({
+  const { issues, counters } = await main({
     ...baseArguments,
     cwd,
   });
 
-  // console.log(issues);
+  assert(issues.binaries['package.json']['eslint']);
+  assert(issues.binaries['package.json']['lint-staged']);
+  assert(issues.devDependencies['package.json']['simple-git-hooks']);
+  assert(issues.devDependencies['package.json']['lint-staged']);
 
   assert.deepEqual(counters, {
     ...baseCounters,
