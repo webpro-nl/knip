cdea5b19f9ffcc0ed6570f6b3081367f466f279b
Aramis Sennyey <159921952+aramissennyeydd@users.noreply.github.com>
2024-07-16 01:18:58 -0400

    feat(cypress): add support for cypress-multi-reporter (#726)
    Signed-off-by: aramissennyeydd <aramis.sennyey@doordash.com>
    Co-authored-by: Lars Kappert <lars@webpro.nl>

---
+++ packages/knip/test/plugins/cypress-multi-reporter.test.ts
@@ -0,0 +1,28 @@
+import { test } from 'bun:test';
+import assert from 'node:assert/strict';
+import { main } from '../../src/index.js';
+import { resolve } from '../../src/util/path.js';
+import baseArguments from '../helpers/baseArguments.js';
+import baseCounters from '../helpers/baseCounters.js';
+
+const cwd = resolve('fixtures/plugins/cypress-multi-reporter');
+
+test('Find dependencies with the cypress-multi-reporter plugin', async () => {
+  const { issues, counters } = await main({
+    ...baseArguments,
+    cwd,
+  });
+
+  assert(issues.unlisted['cypress.config.ts']['@nrwl/cypress/plugins/cypress-preset']);
+  assert(issues.unlisted['cypress/support/commands.ts']['@faker-js/faker']);
+  assert(issues.unlisted['cypress/support/e2e.ts']['@testing-library/cypress/add-commands']);
+  assert(issues.unlisted['cypress.config.ts']['@testing-library/my-fake-reporter']);
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    devDependencies: 0,
+    unlisted: 4,
+    processed: 3,
+    total: 3,
+  });
+});
