953d4fbd4adf993665c5e442baa4142a64b2e37d
Lars Kappert <lars@webpro.nl>
2024-07-19 13:22:19 +0200

    Support override configs in babel plugin

--- packages/knip/src/plugins/babel/types.ts
+++ packages/knip/src/plugins/babel/types.ts
@@ -6,6 +6,7 @@ export type BabelConfigObj = {
   plugins?: (string | [string, unknown])[];
   presets?: (string | [string, unknown])[];
   env?: Record<string, BabelConfigObj>;
+  overrides?: BabelConfigObj[];
 };
 
 export type BabelConfig = BabelConfigObj | BabelConfigFn;
