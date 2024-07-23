3416e5831206d304a4b62a512ceaa6b599f1653f
ChiefORZ <seb.schaffernak@gmail.com>
2024-07-23 15:46:50 +0700

    fix: find the package manager using the git project root

--- packages/create-config/index.js
+++ packages/create-config/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import { execSync } from 'node:child_process';
 import { statSync } from 'node:fs';
+// biome-ignore lint/nursery/noRestrictedImports: ignore
+import path from 'node:path';
 
 const fileExists = filePath => {
   const stat = statSync(filePath, { throwIfNoEntry: false });
@@ -8,9 +10,12 @@ const fileExists = filePath => {
 };
 
 const getPackageManager = () => {
-  if (fileExists('bun.lockb')) return 'bun';
-  if (fileExists('yarn.lock')) return 'yarn';
-  if (fileExists('pnpm-lock.yaml')) return 'pnpm';
+  // get the root of the repository
+  const repositoryRoot = execSync('git rev-parse --show-toplevel').toString().trim();
+
+  if (fileExists(path.join(repositoryRoot, 'bun.lockb'))) return 'bun';
+  if (fileExists(path.join(repositoryRoot, 'yarn.lock'))) return 'yarn';
+  if (fileExists(path.join(repositoryRoot, 'pnpm-lock.yaml'))) return 'pnpm';
   return 'npm';
 };
 
