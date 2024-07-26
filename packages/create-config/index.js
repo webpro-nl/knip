d5003d1b9d4ff9af56712f1f615aee270bdae564
ChiefORZ <seb.schaffernak@gmail.com>
2024-07-26 13:25:39 +0700

    fix: pnpm workspace fixes (#738)

--- packages/create-config/index.js
+++ packages/create-config/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import { execSync } from 'node:child_process';
 import { statSync } from 'node:fs';
+// biome-ignore lint/nursery/noRestrictedImports: ignore
+import path from 'node:path';
 
 const fileExists = filePath => {
   const stat = statSync(filePath, { throwIfNoEntry: false });
@@ -8,9 +10,12 @@ const fileExists = filePath => {
 };
 
 const getPackageManager = () => {
-  if (fileExists('bun.lockb')) return 'bun';
-  if (fileExists('yarn.lock')) return 'yarn';
-  if (fileExists('pnpm-lock.yaml')) return 'pnpm';
+  // get the root of the repository
+  const repositoryRoot = execSync('git rev-parse --show-toplevel').toString().trim();
+
+  if (fileExists(path.join(repositoryRoot, 'bun.lockb'))) return 'bun';
+  if (fileExists(path.join(repositoryRoot, 'yarn.lock'))) return 'yarn';
+  if (fileExists(path.join(repositoryRoot, 'pnpm-lock.yaml'))) return 'pnpm';
   return 'npm';
 };
 
@@ -21,7 +26,11 @@ const main = () => {
   }
 
   const pm = getPackageManager();
-  const cmd = [pm, 'add', '-D', 'knip', 'typescript', '@types/node'].join(' ');
+  // check if pnpm workspace
+  const isPnpmWorkspace = fileExists('pnpm-workspace.yaml');
+  const cmd = [pm, 'add', isPnpmWorkspace ? '-w' : undefined, '-D', 'knip', 'typescript', '@types/node']
+    .filter(Boolean)
+    .join(' ');
 
   execSync(cmd);
   console.info('✓ Install Knip');
