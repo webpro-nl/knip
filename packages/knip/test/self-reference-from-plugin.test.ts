953d6f616155e1addcff9fdcd6b72fbddc1292c8
Lars Kappert <lars@webpro.nl>
2024-07-28 11:26:20 +0200

    Enable more tests in Bun

--- packages/knip/test/self-reference-from-plugin.test.ts
+++ packages/knip/test/self-reference-from-plugin.test.ts
@@ -5,11 +5,9 @@ import { resolve } from '../src/util/path.js';
 import baseArguments from './helpers/baseArguments.js';
 import baseCounters from './helpers/baseCounters.js';
 
-const skipIf = typeof Bun !== 'undefined' ? test.skip : test;
-
 const cwd = resolve('fixtures/self-reference-from-plugin');
 
-skipIf('Allows self-references from plugin', async () => {
+test('Allows self-references from plugin', async () => {
   const { counters } = await main({
     ...baseArguments,
     cwd,
