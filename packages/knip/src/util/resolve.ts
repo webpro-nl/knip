c7fa02e19a18f084c87b82bb24a2c933882b3086
Lars Kappert <lars@webpro.nl>
2024-08-06 08:38:39 +0200

    Increase enhanced-resolve cache duration

--- packages/knip/src/util/resolve.ts
+++ packages/knip/src/util/resolve.ts
@@ -5,7 +5,7 @@ import { timerify } from './Performance.js';
 import { toPosix } from './path.js';
 
 // @ts-ignore error TS2345 (not in latest): Argument of type 'typeof import("node:fs")' is not assignable to parameter of type 'BaseFileSystem'.
-const fileSystem = new ER.CachedInputFileSystem(fs, 9999);
+const fileSystem = new ER.CachedInputFileSystem(fs, 9999999);
 
 export const createSyncResolver = (extensions: string[]) => {
   const resolver = ER.create.sync({
