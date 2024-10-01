6d47630850f09ba2937e4354896a314dabbe6d78
Lars Kappert <lars@webpro.nl>
2024-10-01 20:57:36 +0200

    Remove obsolete jiti options

--- packages/knip/src/util/jiti.ts
+++ packages/knip/src/util/jiti.ts
@@ -1,14 +1,11 @@
 import { fileURLToPath } from 'node:url';
 import { type JitiOptions, createJiti } from 'jiti';
-import { DEFAULT_EXTENSIONS } from '../constants.js';
 import { join } from './path.js';
 
 const empty = join(fileURLToPath(import.meta.url), '../empty.js');
 
 const options = {
   interopDefault: true,
-  extensions: DEFAULT_EXTENSIONS,
-  esmResolve: false,
   alias: {
     '@rushstack/eslint-config/patch/modern-module-resolution': empty,
     '@rushstack/eslint-patch/modern-module-resolution': empty,
