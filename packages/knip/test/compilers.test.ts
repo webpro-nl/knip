c2e27127636943cbad3a79b2d933e51a81e2f25e
Lars Kappert <lars@webpro.nl>
2024-10-23 09:07:01 +0200

    Add coverage for unused files with compiler extension

--- packages/knip/test/compilers.test.ts
+++ packages/knip/test/compilers.test.ts
@@ -1,21 +1,25 @@
 import { test } from 'bun:test';
 import assert from 'node:assert/strict';
 import { main } from '../src/index.js';
-import { resolve } from '../src/util/path.js';
+import { join, resolve } from '../src/util/path.js';
 import baseArguments from './helpers/baseArguments.js';
 import baseCounters from './helpers/baseCounters.js';
 
 const cwd = resolve('fixtures/compilers');
 
 test('Support compiler functions in config', async () => {
-  const { counters } = await main({
+  const { issues, counters } = await main({
     ...baseArguments,
     cwd,
   });
 
+  assert(issues.files.has(join(cwd, 'unused.css')));
+  assert(issues.files.has(join(cwd, 'unused.md')));
+
   assert.deepEqual(counters, {
     ...baseCounters,
-    processed: 7,
-    total: 7,
+    files: 2,
+    processed: 9,
+    total: 9,
   });
 });
