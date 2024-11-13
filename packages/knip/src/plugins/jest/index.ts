153a8368de35237c2edb3ebf464f2552da525c72
Tryggvi Gylfason <tryggvi@spotify.com>
2024-11-13 20:59:36 +0100

    jest: support custom testEnvironment (#834)

--- packages/knip/src/plugins/jest/index.ts
+++ packages/knip/src/plugins/jest/index.ts
@@ -58,7 +58,12 @@ const resolveDependencies = async (config: JestInitialOptions, options: PluginOp
 
   const runner = config.runner ? [config.runner] : [];
   const runtime = config.runtime && config.runtime !== 'jest-circus' ? [config.runtime] : [];
-  const environments = config.testEnvironment === 'jsdom' ? ['jest-environment-jsdom'] : [];
+  const environments =
+    config.testEnvironment === 'jsdom'
+      ? ['jest-environment-jsdom']
+      : config.testEnvironment
+        ? [config.testEnvironment]
+        : [];
   const resolvers = config.resolver ? [config.resolver] : [];
   const reporters = config.reporters
     ? config.reporters
