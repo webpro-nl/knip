d34e32eb1957983c9a1a86bf4e570c5c9815b87b
heystewart <heyistewart@gmail.com>
2025-01-04 15:21:54 -0500

    format ... an eslintrc would be nice

--- packages/knip/test/import-star-iteration.ts
+++ packages/knip/test/import-star-iteration.ts
@@ -1,13 +1,13 @@
-import { test } from "bun:test";
-import assert from "node:assert/strict";
-import { main as knip } from "../src/index.js";
-import { resolve } from "../src/util/path.js";
-import baseArguments from "./helpers/baseArguments.js";
+import { test } from 'bun:test';
+import assert from 'node:assert/strict';
+import { main as knip } from '../src/index.js';
+import { resolve } from '../src/util/path.js';
+import baseArguments from './helpers/baseArguments.js';
 import baseCounters from './helpers/baseCounters.js';
 
-const cwd = resolve("fixtures/import-star-iteration");
+const cwd = resolve('fixtures/import-star-iteration');
 
-test("Handle usage of members of a namespace when imported using * and iterating", async () => {
+test('Handle usage of members of a namespace when imported using * and iterating', async () => {
   const { counters } = await knip({
     ...baseArguments,
     cwd,
