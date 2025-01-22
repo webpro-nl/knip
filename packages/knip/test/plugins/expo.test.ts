c121d44efb33195174a569b7ef23b960c9118544
Lars Kappert <lars@webpro.nl>
2025-01-22 16:14:55 +0100

    Always use `production` entries by default in expo plugin

--- packages/knip/test/plugins/expo.test.ts
+++ packages/knip/test/plugins/expo.test.ts
@@ -1,7 +1,7 @@
 import { test } from 'bun:test';
 import assert from 'node:assert/strict';
 import { main } from '../../src/index.js';
-import { join, resolve } from '../../src/util/path.js';
+import { resolve } from '../../src/util/path.js';
 import baseArguments from '../helpers/baseArguments.js';
 import baseCounters from '../helpers/baseCounters.js';
 
@@ -13,15 +13,12 @@ test('Find dependencies with the Expo plugin (1)', async () => {
     cwd,
   });
 
-  assert(issues.files.has(join(cwd, 'src/app/index.ts')));
-
   assert(issues.dependencies['package.json']['expo-router']);
 
   assert.deepEqual(counters, {
     ...baseCounters,
     processed: 2,
     total: 2,
-    files: 1,
     dependencies: 1,
   });
 });
