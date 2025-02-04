572282598af51d80ed4eee9f0b355a9ffac3e971
Lasse Narula <lasse.klovstad@gmail.com>
2025-02-04 20:42:56 +0100

    Adjust fixture to test relative route file

--- packages/knip/fixtures/plugins/react-router/app/routes.ts
+++ packages/knip/fixtures/plugins/react-router/app/routes.ts
@@ -2,6 +2,6 @@ export default [
   { file: "routes/home.tsx", index: true },
   {
     file: "routes/layout.tsx",
-    children: [{ file: "routes/another-route.tsx" }],
+    children: [{ file: "./routes/another-route.tsx" }],
   },
 ];
