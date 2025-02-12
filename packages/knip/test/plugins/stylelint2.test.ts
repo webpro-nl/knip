695c01d4c4b32b2aaaa989ae073a34d4c81df41d
TJNhxMZHmqGytuWT <113251528+TJNhxMZHmqGytuWT@users.noreply.github.com>
2025-02-12 01:21:18 +0100

    remove issues in tests

--- packages/knip/test/plugins/stylelint2.test.ts
+++ packages/knip/test/plugins/stylelint2.test.ts
@@ -8,21 +8,13 @@ import baseCounters from '../helpers/baseCounters.js';
 const cwd = resolve('fixtures/plugins/stylelint2');
 
 test('Find dependencies with the stylelint plugin (2)', async () => {
-  const { issues, counters } = await main({
+  const { counters } = await main({
     ...baseArguments,
     cwd,
   });
 
-  assert(issues.dependencies['package.json']['stylelint-order']);
-  assert(issues.unresolved['stylelint.config.js']['stylelint-config-standard']);
-  assert(issues.unlisted['stylelint.config.js']['stylelint-config-recommended']);
-  assert(issues.unresolved['.stylelintrc.mjs']['stylelint-config-standard']);
-
   assert.deepEqual(counters, {
     ...baseCounters,
-    dependencies: 1,
-    unlisted: 1,
-    unresolved: 2,
     processed: 4,
     total: 4,
   });
