e03478fac5b0f53dbdd47d3b03c03609e036c4e6
Lars Kappert <lars@webpro.nl>
2025-03-24 17:31:51 +0100

    Add plugin-ast test + fixture + some removeFromAST

---
+++ packages/knip/fixtures/plugin-ast/routes/index.ts
@@ -0,0 +1,1 @@
+import '@tanstack/react-router';
---
+++ packages/knip/test/plugin-ast.test.ts
@@ -0,0 +1,29 @@
+import { test } from 'bun:test';
+import assert from 'node:assert/strict';
+import { main } from '../src/index.js';
+import { resolve } from '../src/util/path.js';
+import baseArguments from './helpers/baseArguments.js';
+import baseCounters from './helpers/baseCounters.js';
+
+const cwd = resolve('fixtures/plugin-ast');
+
+test('Allow plugins to implement resolveEntryPathsFromAST and get inputs from source file AST', async () => {
+  const { issues, counters } = await main({
+    ...baseArguments,
+    cwd,
+  });
+
+  assert(issues.unlisted['handlers/auth.handler.ts']['sst-auth-handler-dep']);
+  assert(issues.unlisted['handlers/auth.ts']['sst-auth-dep']);
+  assert(issues.unlisted['stacks/AuthStack.ts']['sst-auth-stack-dep']);
+  assert(issues.unlisted['stacks/AuthHandlerStack.ts']['sst-auth-handler-stack-dep']);
+  assert(issues.unlisted['handlers/some-route-handler.ts']['sst-some-dep']);
+  assert(issues.unlisted['sst.config.ts']['sst-config-dep']);
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    unlisted: 7,
+    total: 11,
+    processed: 11,
+  });
+});
