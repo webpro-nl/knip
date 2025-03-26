98a00f6abe84ca113189c68419b78babb42031d1
Lars Kappert <lars@webpro.nl>
2025-03-26 11:51:25 +0100

    Add AST helpers and use in plugins

--- packages/knip/src/plugins/next/resolveFromAST.ts
+++ packages/knip/src/plugins/next/resolveFromAST.ts
@@ -1,23 +1,13 @@
 import ts from 'typescript';
-import { stripQuotes } from '../../typescript/ast-helpers.js';
+import { getPropertyValues } from '../../typescript/ast-helpers.js';
 
 export const getPageExtensions = (sourceFile: ts.SourceFile) => {
   const pageExtensions: Set<string> = new Set();
 
   function visit(node: ts.Node) {
     if (ts.isObjectLiteralExpression(node)) {
-      const pageExtensionsProp = node.properties.find(
-        prop => ts.isPropertyAssignment(prop) && prop.name.getText() === 'pageExtensions'
-      );
-
-      if (pageExtensionsProp && ts.isPropertyAssignment(pageExtensionsProp)) {
-        const initializer = pageExtensionsProp.initializer;
-        if (ts.isArrayLiteralExpression(initializer)) {
-          for (const element of initializer.elements) {
-            if (ts.isStringLiteral(element)) pageExtensions.add(stripQuotes(element.getText()));
-          }
-        }
-      }
+      const values = getPropertyValues(node, 'pageExtensions');
+      for (const value of values) pageExtensions.add(value);
     }
 
     ts.forEachChild(node, visit);
