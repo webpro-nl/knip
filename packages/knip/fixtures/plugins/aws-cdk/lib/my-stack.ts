88d6d877a4de8ff038ce9bbcaf017d63e8a926bb
thoroc <thomas.a.roche@gmail.com>
2025-04-21 13:46:40 +0100

    fix: example on how to import the construct

--- packages/knip/fixtures/plugins/aws-cdk/lib/my-stack.ts
+++ packages/knip/fixtures/plugins/aws-cdk/lib/my-stack.ts
@@ -2,10 +2,13 @@
 import * as cdk from 'aws-cdk-lib';
 // @ts-ignore
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import { MyConstruct } from './my-construct';
 
 export class MyStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string) {
     super(scope, id);
+
+    const newConstruct = new MyConstruct(this, 'MyConstruct');
 
     // The code that defines your stack goes here
     new s3.Bucket(this, 'MyFirstBucket', {
