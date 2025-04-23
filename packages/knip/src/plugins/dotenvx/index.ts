00e01f6d9fca7d10822b27b999555d8663b81fc6
Robin van der Vleuten <robin@webstronauts.com>
2025-04-23 14:08:23 +0200

    format

--- packages/knip/src/plugins/dotenvx/index.ts
+++ packages/knip/src/plugins/dotenvx/index.ts
@@ -1,5 +1,4 @@
 import type { ParsedArgs } from 'minimist';
-import { argsFrom } from '../../binaries/util.js';
 import type { Plugin } from '../../types/config.js';
 
 // https://dotenvx.com/
@@ -8,7 +7,7 @@ const title = 'dotenvx';
 
 const args = {
   fromArgs: (parsed: ParsedArgs) => (parsed._[0] === 'run' ? parsed._.slice(1) : []),
-}
+};
 
 export default {
   title,
