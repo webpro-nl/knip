89cde0b05ee78d71423647989e0ab515bec1bb84
Lars Kappert <lars@webpro.nl>
2025-04-24 21:36:44 +0200

    Fix order and add types in codeclimate specs

--- packages/knip/test/helpers/assertAndRemoveProperty.ts
+++ packages/knip/test/helpers/assertAndRemoveProperty.ts
@@ -1,3 +1,6 @@
+import assert from 'node:assert/strict';
+import type { Issue, Location } from 'codeclimate-types';
+
 function assertAndRemoveProperty<TIn extends object, TProp extends keyof TIn>(
   obj: TIn,
   propName: TProp,
@@ -5,8 +8,18 @@ function assertAndRemoveProperty<TIn extends object, TProp extends keyof TIn>(
 ): Omit<TIn, TProp> {
   const { [propName]: value, ...rest } = obj;
   assertProperty(value);
-
   return rest;
 }
 
-export { assertAndRemoveProperty };
+export function assertAndRemoveFingerprint(issue: Issue) {
+  return assertAndRemoveProperty(issue, 'fingerprint', fingerprint => assert.match(fingerprint, /[a-f0-9]{32}/));
+}
+
+const getBeginLine = (loc: Location): number =>
+  'lines' in loc ? loc.lines.begin : 'line' in loc.positions.begin ? loc.positions.begin.line : 0;
+
+export function orderByPos(a: Issue, b: Issue): number {
+  const pathCompare = a.location.path.localeCompare(b.location.path);
+  if (pathCompare !== 0) return pathCompare;
+  return getBeginLine(a.location) - getBeginLine(b.location);
+}
