8d855d21dd47816b76f9c717cf1af6d595c91c99
chanakorn <chanakorn.aramsak@gmail.com>
2025-05-09 22:27:50 +0700

    Add mock fixtures

--- packages/knip/fixtures/plugins/vite/vite.config.ts
+++ packages/knip/fixtures/plugins/vite/vite.config.ts
@@ -2,6 +2,9 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  resolve: {
+    extensions: ['.js'],
+  },
   plugins: [
     react({
       babel: {
---
+++ packages/knip/fixtures/plugins/vite2/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+const isDesktop = process.env.build_type === 'desktop';
+
+function getExtensions() {
+  const extensions = ['.js', '.ts', '.tsx', '.json'];
+  if (isDesktop) {
+    return [...extensions.map((ext) => `.desktop${ext}`), ...extensions];
+  }
+
+  return extensions;
+};
+
+export default defineConfig({
+  resolve: {
+    extensions: getExtensions(),
+  },
+})
