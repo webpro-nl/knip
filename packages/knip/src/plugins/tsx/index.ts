8e7045277c77becb945b18a4819146ffa2d1d146
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-16 14:07:11 -0600

    detect test files if tsx --test script is present

--- packages/knip/src/plugins/tsx/index.ts
+++ packages/knip/src/plugins/tsx/index.ts
@@ -1,8 +1,37 @@
-import type { Plugin } from '../../types/config.js';
+import type { PackageJson } from 'src/types/package-json.js';
+import { toBinary, toDependency, toEntry } from 'src/util/input.js';
+import { hasDependency } from 'src/util/plugin.js';
+import type { IsPluginEnabled, Plugin, ResolveConfig } from '../../types/config.js';
 
 // https://tsx.is
 
 const title = 'tsx';
+
+const enablers = ['tsx'];
+
+const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependencies, enablers);
+
+const config = ['package.json'];
+
+const packageJsonPath = (id: PackageJson) => id;
+
+const resolveConfig: ResolveConfig<PackageJson> = localConfig => {
+  const scripts = localConfig.scripts;
+
+  const entries = [toBinary('tsx'), toDependency('tsx')];
+
+  if (scripts && Object.keys(scripts).some(script => /(?<=^|\s)tsx\s(.*)--test/.test(scripts[script]))) {
+    const patterns = [
+      '**/*{.,-,_}test.?(c|m)(j|t)s',
+      '**/test-*.?(c|m)(j|t)s',
+      '**/test.?(c|m)(j|t)s',
+      '**/test/**/*.?(c|m)(j|t)s',
+    ];
+    entries.push(...patterns.map(id => toEntry(id)));
+  }
+
+  return entries;
+};
 
 const args = {
   positional: true,
@@ -12,5 +41,9 @@ const args = {
 
 export default {
   title,
+  isEnabled,
+  packageJsonPath,
+  config,
+  resolveConfig,
   args,
 } satisfies Plugin;
