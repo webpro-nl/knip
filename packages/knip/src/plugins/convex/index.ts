4eee77d3c6beb20d778ac2fa3cc52d161861b4f6
Nathan Arthur <nathan@nathanarthur.com>
2025-05-17 10:19:13 -0400

    fix: remove unnecessary code from convex plugin

--- packages/knip/src/plugins/convex/index.ts
+++ packages/knip/src/plugins/convex/index.ts
@@ -1,33 +1,23 @@
-import type { IsPluginEnabled, Plugin, ResolveConfig } from '../../types/config.js';
-import { toDeferResolve } from '../../util/input.js';
-import { hasDependency } from '../../util/plugin.js';
-import type { PluginConfig } from './types.js';
+import type { IsPluginEnabled, Plugin } from "../../types/config.js";
+import { hasDependency } from "../../util/plugin.js";
 
 // https://docs.convex.dev/home
 
-const title = 'Convex';
+const title = "Convex";
 
-const enablers = ['convex'];
+const enablers = ["convex"];
 
-const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependencies, enablers);
+const isEnabled: IsPluginEnabled = ({ dependencies }) =>
+  hasDependency(dependencies, enablers);
 
-const config: string[] = [];
-
-const entry: string[] = ['convex/*.config.@(js|jsx|ts|tsx)', 'convex/_generated/*.@(js|jsx|ts|tsx)'];
-
-const production: string[] = [];
-
-const resolveConfig: ResolveConfig<PluginConfig> = async config => {
-  const inputs = config?.plugins ?? [];
-  return [...inputs].map(toDeferResolve);
-};
+const entry: string[] = [
+  "convex/*.config.@(js|jsx|ts|tsx)",
+  "convex/_generated/*.@(js|jsx|ts|tsx)",
+];
 
 export default {
   title,
   enablers,
   isEnabled,
-  config,
   entry,
-  production,
-  resolveConfig,
 } satisfies Plugin;
