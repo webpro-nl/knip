9284bfa93b1fe8bc5106c1fb919fb8de240da7df
Nathan Arthur <nathan@nathanarthur.com>
2025-05-17 10:22:21 -0400

    fix: format convex plugin

--- packages/knip/src/plugins/convex/index.ts
+++ packages/knip/src/plugins/convex/index.ts
@@ -1,19 +1,15 @@
-import type { IsPluginEnabled, Plugin } from "../../types/config.js";
-import { hasDependency } from "../../util/plugin.js";
+import type { IsPluginEnabled, Plugin } from '../../types/config.js';
+import { hasDependency } from '../../util/plugin.js';
 
 // https://docs.convex.dev/home
 
-const title = "Convex";
+const title = 'Convex';
 
-const enablers = ["convex"];
+const enablers = ['convex'];
 
-const isEnabled: IsPluginEnabled = ({ dependencies }) =>
-  hasDependency(dependencies, enablers);
+const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependencies, enablers);
 
-const entry: string[] = [
-  "convex/*.config.@(js|jsx|ts|tsx)",
-  "convex/_generated/*.@(js|jsx|ts|tsx)",
-];
+const entry: string[] = ['convex/*.config.@(js|jsx|ts|tsx)', 'convex/_generated/*.@(js|jsx|ts|tsx)'];
 
 export default {
   title,
