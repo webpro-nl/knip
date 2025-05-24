aae9d03c44c4b8eef45113aa6d434d1942a68187
chanakorn <chanakorn.aramsak@gmail.com>
2025-05-24 14:16:15 +0700

    Update test

--- packages/knip/fixtures/plugins/vite2/vite.config.ts
+++ packages/knip/fixtures/plugins/vite2/vite.config.ts
@@ -1,15 +1,18 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+const isDesktop = process.env.build_type === 'desktop';
+function getExtensions() {
+  const extensions = ['.js', '.ts', '.tsx', '.json'];
+  if (isDesktop) {
+    return [...extensions.map(ext => `.desktop${ext}`), ...extensions];
+  }
+
+  return extensions;
+}
 
 export default defineConfig({
   resolve: {
-    extensions: ['.js'],
+    extensions: getExtensions(),
   },
-  plugins: [
-    react({
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),
-  ],
 });
--- packages/knip/test/plugins/vite2.test.ts
+++ packages/knip/test/plugins/vite2.test.ts
@@ -6,40 +6,21 @@ import baseArguments from '../helpers/baseArguments.js';
 import baseCounters from '../helpers/baseCounters.js';
 
 const cwd = resolve('fixtures/plugins/vite2');
-const cwd2 = resolve('fixtures/plugins/vite3');
-test('Find extension issues with incomplete config', async () => {
-  const { issues, counters } = await main({
+
+test('Should not find issues with extensions when build_type is desktop', async () => {
+  // Set build_type to desktop for this test
+  process.env.build_type = 'desktop';
+  const { counters } = await main({
     ...baseArguments,
     cwd,
   });
-
-  assert(issues.files.has(join(cwd, 'src/mock.ts')));
-  assert(issues.files.has(join(cwd, 'src/mock.desktop.ts')));
-
-  assert.deepEqual(counters, {
-    ...baseCounters,
-    files: 2,
-    devDependencies: 0,
-    unlisted: 0,
-    processed: 6,
-    total: 6,
-  });
-});
-
-test('Should find 0 issues with proper extensions config', async () => {
-  const { issues, counters } = await main({
-    ...baseArguments,
-    cwd: cwd2,
-  });
-
-  assert.strictEqual(issues.files.size, 0);
 
   assert.deepEqual(counters, {
     ...baseCounters,
     files: 0,
     devDependencies: 2,
     unlisted: 0,
-    processed: 5,
-    total: 5,
+    processed: 3,
+    total: 3,
   });
 });
