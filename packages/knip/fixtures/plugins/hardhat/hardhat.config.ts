672f90980586af0b3db72b35d4be7de19191633e
Nick Barry <itsnickbarry@protonmail.ch>
2025-06-03 16:16:37 -0600

    detect hardhat imports in dependenciesToCompile configuration

--- packages/knip/fixtures/plugins/hardhat/hardhat.config.ts
+++ packages/knip/fixtures/plugins/hardhat/hardhat.config.ts
@@ -1,3 +1,9 @@
-const config = {};
+import type { HardhatUserConfig } from "../../../src/plugins/hardhat/types.js";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    dependenciesToCompile: ["@solidstate/contracts/interfaces/IERC20.sol"],
+  },
+};
 
 export default config;
--- packages/knip/src/plugins/hardhat/index.ts
+++ packages/knip/src/plugins/hardhat/index.ts
@@ -1,19 +1,41 @@
-import type { IsPluginEnabled, Plugin, Resolve } from '../../types/config.js';
-import { toDependency } from '../../util/input.js';
-import { hasDependency } from '../../util/plugin.js';
+import type {
+  IsPluginEnabled,
+  Plugin,
+  Resolve,
+  ResolveConfig,
+} from "../../types/config.js";
+import { toDeferResolve, toDependency } from "../../util/input.js";
+import { hasDependency } from "../../util/plugin.js";
+import type { HardhatUserConfig } from "./types.js";
 
 // https://hardhat.org/docs
 
-const title = 'Hardhat';
+const title = "Hardhat";
 
-const enablers = ['hardhat'];
+const enablers = ["hardhat"];
 
-const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependencies, enablers);
+const isEnabled: IsPluginEnabled = ({ dependencies }) =>
+  hasDependency(dependencies, enablers);
 
-const entry: string[] = ['hardhat.config.{js,cjs,mjs,ts}'];
+const entry: string[] = ["hardhat.config.{js,cjs,mjs,ts}"];
+
+const config = ["hardhat.config.{js,cjs,mjs,ts}"];
 
 const resolve: Resolve = async () => {
-  return [toDependency('hardhat')];
+  return [toDependency("hardhat")];
+};
+
+const resolveConfig: ResolveConfig<HardhatUserConfig> = (config) => {
+  return [config.solidity]
+    .flat()
+    .filter(
+      (solidityConfig) =>
+        typeof solidityConfig !== "undefined" &&
+        typeof solidityConfig !== "string",
+    )
+    .map((solidityConfig) => solidityConfig.dependenciesToCompile ?? [])
+    .flat()
+    .map(toDeferResolve);
 };
 
 export default {
@@ -21,5 +43,7 @@ export default {
   enablers,
   isEnabled,
   entry,
+  config,
   resolve,
+  resolveConfig,
 } satisfies Plugin;
