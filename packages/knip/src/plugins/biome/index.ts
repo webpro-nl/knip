d620a9ad7b04a301fd58e75a73fcf672687c192d
Vinh Tran <vinh@calif.io>
2025-06-06 16:02:04 -0400

    Reformat

--- packages/knip/src/plugins/biome/index.ts
+++ packages/knip/src/plugins/biome/index.ts
@@ -1,5 +1,5 @@
 import type { IsPluginEnabled, Plugin, PluginOptions, Resolve, ResolveConfig } from '../../types/config.js';
-import { type Input, toConfig, toBinary, toDependency } from '../../util/input.js';
+import { type Input, toConfig, toDependency } from '../../util/input.js';
 import { hasDependency } from '../../util/plugin.js';
 import type { BiomeConfig } from './types.js';
 
@@ -16,18 +16,15 @@ const config: string[] = ['biome.json', 'biome.jsonc'];
 
 const resolveExtends = (extendsArray: string[], options: PluginOptions): Input[] => {
   return extendsArray.map(specifier => {
-   if (specifier.endsWith('.json') || specifier.endsWith('.jsonc')) {
-    return toConfig('biome', specifier)
-   } else {
-    return toConfig('biome', specifier, { containingFilePath: options.configFilePath })
-   } 
+    if (specifier.endsWith('.json') || specifier.endsWith('.jsonc')) {
+      return toConfig('biome', specifier);
+    }
+    return toConfig('biome', specifier, { containingFilePath: options.configFilePath });
   });
 };
 
 const resolveConfig: ResolveConfig<BiomeConfig> = (config, options) => {
-  return [
-    ...resolveExtends(config.extends || [], options),
-  ];
+  return [...resolveExtends(config.extends || [], options)];
 };
 
 const resolve: Resolve = options => {
