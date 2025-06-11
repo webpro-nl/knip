1d54af76e0b5c44071bbf84c29e7c2b65c8bef8a
Nathan Arthur <nathan@nathanarthur.com>
2025-06-11 08:32:13 -0400

    fix: link to hey-api docs

--- packages/knip/src/plugins/hey-api/index.ts
+++ packages/knip/src/plugins/hey-api/index.ts
@@ -1,15 +1,20 @@
-import type { IsPluginEnabled, Plugin, ResolveConfig } from '../../types/config.js';
-import { toDeferResolve } from '../../util/input.js';
-import { hasDependency } from '../../util/plugin.js';
-import type { PluginConfig } from './types.js';
+import type {
+  IsPluginEnabled,
+  Plugin,
+  ResolveConfig,
+} from "../../types/config.js";
+import { toDeferResolve } from "../../util/input.js";
+import { hasDependency } from "../../util/plugin.js";
+import type { PluginConfig } from "./types.js";
 
-// link to hey-api docs
+// https://heyapi.dev/openapi-ts/get-started
 
-const title = 'hey-api';
+const title = "hey-api";
 
-const enablers = ['hey-api'];
+const enablers = ["hey-api"];
 
-const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependencies, enablers);
+const isEnabled: IsPluginEnabled = ({ dependencies }) =>
+  hasDependency(dependencies, enablers);
 
 const config: string[] = [];
 
@@ -17,7 +22,7 @@ const entry: string[] = [];
 
 const production: string[] = [];
 
-const resolveConfig: ResolveConfig<PluginConfig> = async config => {
+const resolveConfig: ResolveConfig<PluginConfig> = async (config) => {
   const inputs = config?.plugins ?? [];
   return [...inputs].map(toDeferResolve);
 };
