f2be1f65f24c6eea0f0a37b0bff596be4f02d6cb
Alem Tuzlak <t.zlak@hotmail.com>
2025-06-11 13:06:45 +0200

    failing test fix and comment

--- packages/knip/test/plugins/react-router.test.ts
+++ packages/knip/test/plugins/react-router.test.ts
@@ -1,9 +1,12 @@
 import { test } from 'bun:test';
 import assert from 'node:assert/strict';
+import os from 'node:os';
 import { main } from '../../src/index.js';
 import { resolve } from '../../src/util/path.js';
 import baseArguments from '../helpers/baseArguments.js';
 import baseCounters from '../helpers/baseCounters.js';
+
+const isWindows = os.platform() === 'win32';
 
 const cwd = resolve('fixtures/plugins/react-router');
 
@@ -17,5 +20,8 @@ test('Find dependencies with the react-router plugin', async () => {
     ...baseCounters,
     processed: 9,
     total: 9,
+    // There is a bug with routes that include () on Windows so they will not be found there, revert when
+    // the bug is fixed
+    files: isWindows ? 0 : 1,
   });
 });
