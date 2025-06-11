de771d65cb77a61cfa32f7230416ee6f1f2056cf
Alem Tuzlak <t.zlak@hotmail.com>
2025-06-11 12:17:16 +0200

    chore: reverted windows test, added suggestions

--- packages/knip/src/plugins/react-router/index.ts
+++ packages/knip/src/plugins/react-router/index.ts
@@ -1,12 +1,13 @@
 import { existsSync } from 'node:fs';
+import os from 'node:os';
 import type { IsPluginEnabled, Plugin, ResolveConfig } from '../../types/config.js';
 import { toEntry } from '../../util/input.js';
 import { join } from '../../util/path.js';
 import { hasDependency, load } from '../../util/plugin.js';
 import vite from '../vite/index.js';
 import type { PluginConfig, RouteConfigEntry } from './types.js';
-import os from 'node:os';
 
+const isWindows = os.platform() === 'win32';
 // https://reactrouter.com/start/framework/routing
 
 const title = 'React Router';
@@ -48,7 +49,7 @@ const resolveConfig: ResolveConfig<PluginConfig> = async (localConfig, options) 
     // See:
     //  - https://reactrouter.com/how-to/file-route-conventions#optional-segments
     //  - https://www.npmjs.com/package/fast-glob#advanced-syntax
-    .map(route => os.platform() === "win32" ? route : route.replace(/[$^*+?()\[\]]/g, '\\$&'));
+    .map(route => (isWindows ? route : route.replace(/[$^*+?()\[\]]/g, '\\$&')));
 
   return [
     join(appDir, 'routes.{js,ts}'),
--- packages/knip/test/plugins/react-router.test.ts
+++ packages/knip/test/plugins/react-router.test.ts
@@ -1,6 +1,5 @@
 import { test } from 'bun:test';
 import assert from 'node:assert/strict';
-import os from 'node:os';
 import { main } from '../../src/index.js';
 import { resolve } from '../../src/util/path.js';
 import baseArguments from '../helpers/baseArguments.js';
@@ -8,9 +7,7 @@ import baseCounters from '../helpers/baseCounters.js';
 
 const cwd = resolve('fixtures/plugins/react-router');
 
-const skipIfWindows = os.platform() === 'win32' ? test.skip : test;
-
-skipIfWindows('Find dependencies with the react-router plugin', async () => {
+test('Find dependencies with the react-router plugin', async () => {
   const { counters } = await main({
     ...baseArguments,
     cwd,
