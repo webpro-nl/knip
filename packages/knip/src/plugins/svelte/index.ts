e43d414e0ca43b9e4ae826e094cc3ebabf495376
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-07-02 19:31:24 +0200

    Handle SvelteKit service worker integration

--- packages/knip/src/plugins/svelte/index.ts
+++ packages/knip/src/plugins/svelte/index.ts
@@ -18,11 +18,13 @@ const production = [
   'src/routes/**/+{page,server,page.server,error,layout,layout.server}{,@*}.{js,ts,svelte}',
   'src/hooks.{server,client}.{js,ts}',
   'src/params/*.{js,ts}',
+  'src/service-worker{.js,.ts,/index.{js,ts}}',
 ];
 
 const resolve: Resolve = options => {
-  const alias = toAlias('$app/*', [join(options.cwd, 'node_modules/@sveltejs/kit/src/runtime/app/*')]);
-  return [alias];
+  const app = toAlias('$app/*', [join(options.cwd, 'node_modules/@sveltejs/kit/src/runtime/app/*')]);
+  const sw = toAlias('$service-worker', [join(options.cwd, 'node_modules/@sveltejs/kit/types/index.d.ts')]);
+  return [app, sw];
 };
 
 export default {
