f4d41d63d57972bb7857d9e008e4725f322e36a4
Thomascogez <thomas.cogez@gmail.com>
2025-07-16 21:16:40 +0200

    chore: perform only partial resolution of root biome config in `resolveExtends`

--- packages/knip/src/plugins/biome/index.ts
+++ packages/knip/src/plugins/biome/index.ts
@@ -1,9 +1,8 @@
 import type { IsPluginEnabled, Plugin, PluginOptions, ResolveConfig } from '../../types/config.js';
 import { arrayify } from '../../util/array.js';
 import { type Input, toConfig } from '../../util/input.js';
-import { extname, join } from '../../util/path.js';
+import { join } from '../../util/path.js';
 import { hasDependency } from '../../util/plugin.js';
-import { _createSyncResolver } from '../../util/resolve.js';
 import type { BiomeConfig } from './types.js';
 
 const title = 'Biome';
@@ -13,23 +12,15 @@ const enablers = ['@biomejs/biome'];
 const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependencies, enablers);
 
 const config: string[] = ['biome.json', 'biome.jsonc'];
-const configExtensions = config.map(configFileName => extname(configFileName));
-
-const resolveRootConfigReference = (specifier: string, baseDir: string) => {
-  const resolver = _createSyncResolver(configExtensions);
-  const resolvedPath = resolver(specifier, baseDir);
-
-  return resolvedPath;
-};
 
 const isRootConfigReference = (specifier: string) => specifier === '//';
 
 const resolveExtends = (extendsArray: string[], options: PluginOptions): Input[] => {
   return extendsArray.map(specifier => {
     if (isRootConfigReference(specifier)) {
-      const resolvedSpecifier = resolveRootConfigReference(join(options.rootCwd, 'biome'), options.rootCwd);
-      return toConfig('biome', resolvedSpecifier ?? specifier, { containingFilePath: options.configFilePath });
+      return toConfig('biome', join(options.rootCwd, 'biome'), { containingFilePath: options.configFilePath });
     }
+
     return toConfig('biome', specifier, { containingFilePath: options.configFilePath });
   });
 };
