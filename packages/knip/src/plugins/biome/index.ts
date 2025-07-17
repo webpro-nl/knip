07439ef3af85dab40df7596f2c0ae5d392e16b3f
Thomas.CA <44041651+Thomascogez@users.noreply.github.com>
2025-07-17 10:52:27 +0200

    Support new biome config extends definition  (#1177)

--- packages/knip/src/plugins/biome/index.ts
+++ packages/knip/src/plugins/biome/index.ts
@@ -1,5 +1,7 @@
 import type { IsPluginEnabled, Plugin, PluginOptions, ResolveConfig } from '../../types/config.js';
+import { arrayify } from '../../util/array.js';
 import { type Input, toConfig } from '../../util/input.js';
+import { join } from '../../util/path.js';
 import { hasDependency } from '../../util/plugin.js';
 import type { BiomeConfig } from './types.js';
 
@@ -11,12 +13,20 @@ const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependenc
 
 const config: string[] = ['biome.json', 'biome.jsonc'];
 
+const isRootConfigReference = (specifier: string) => specifier === '//';
+
 const resolveExtends = (extendsArray: string[], options: PluginOptions): Input[] => {
-  return extendsArray.map(specifier => toConfig('biome', specifier, { containingFilePath: options.configFilePath }));
+  return extendsArray.map(specifier => {
+    if (isRootConfigReference(specifier)) {
+      return toConfig('biome', join(options.rootCwd, 'biome'), { containingFilePath: options.configFilePath });
+    }
+
+    return toConfig('biome', specifier, { containingFilePath: options.configFilePath });
+  });
 };
 
 const resolveConfig: ResolveConfig<BiomeConfig> = (config, options) => {
-  return [...resolveExtends(config.extends || [], options)];
+  return [...resolveExtends(arrayify(config.extends), options)];
 };
 
 export default {
