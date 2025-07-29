fb1b646ede6488017490d35e8ee38bee8c2a00c2
Ivo Evans <ivo.evansstorrie@rvu.co.uk>
2025-07-29 14:29:50 +0100

    Don't assert on private properties

--- packages/knip/test/plugins/astro.test.ts
+++ packages/knip/test/plugins/astro.test.ts
@@ -4,6 +4,7 @@ import { main } from '../../src/index.js';
 import { resolve } from '../../src/util/path.js';
 import baseArguments from '../helpers/baseArguments.js';
 import baseCounters from '../helpers/baseCounters.js';
+import { join } from '../../src/util/path.js';
 
 const cwd = resolve('fixtures/plugins/astro');
 
@@ -15,12 +16,12 @@ test('Find dependencies with the Astro plugin', async () => {
 
   assert(issues.exports['src/consts.ts']['UNUSED']);
 
-  assert(issues._files['fixtures/plugins/astro/src/pages/_top-level-file-unused.ts']);
-  assert(issues._files['fixtures/plugins/astro/src/pages/_top-level-dir-unused/index.ts']);
+  assert(issues.files.has(join(cwd, 'src/pages/_top-level-file-unused.ts')));
+  assert(issues.files.has(join(cwd, 'src/pages/_top-level-dir-unused/index.ts')));
 
-  assert(issues._files['fixtures/plugins/astro/src/pages/blog/_nested-unused-file.ts']);
-  assert(issues._files['fixtures/plugins/astro/src/pages/blog/_util/unused-component.astro']);
-  assert(issues._files['fixtures/plugins/astro/src/pages/blog/_util/nested/deeply-nested-unused-file.ts']);
+  assert(issues.files.has(join(cwd, 'src/pages/blog/_nested-unused-file.ts')));
+  assert(issues.files.has(join(cwd, 'src/pages/blog/_util/unused-component.astro')));
+  assert(issues.files.has(join(cwd, 'src/pages/blog/_util/nested/deeply-nested-unused-file.ts')));
 
   assert.deepEqual(counters, {
     ...baseCounters,
