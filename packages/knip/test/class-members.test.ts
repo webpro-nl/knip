e132ab595b73bb840630d926a8a80ed9d4e46123
Desuuuu <contact@desuuuu.com>
2025-07-30 07:14:26 +0200

    Add option to ignore class member implementations (#1174)

--- packages/knip/test/class-members.test.ts
+++ packages/knip/test/class-members.test.ts
@@ -15,6 +15,7 @@ test('Find unused class members', async () => {
 
   assert.equal(Object.keys(issues.classMembers['members.ts']).length, 6);
   assert(issues.classMembers['iterator-generator.ts']['AbstractClassGen.unimplemented']);
+  assert(issues.classMembers['iterator.ts']['AbstractClass.implemented']);
   assert(issues.classMembers['members.ts']['MyClass.bUnusedPublic']);
   assert(issues.classMembers['members.ts']['MyClass.cUnusedProp']);
   assert(issues.classMembers['members.ts']['MyClass.dUnusedMember']);
@@ -24,7 +25,7 @@ test('Find unused class members', async () => {
 
   assert.deepEqual(counters, {
     ...baseCounters,
-    classMembers: 7,
+    classMembers: 8,
     processed: 5,
     total: 5,
   });
@@ -39,6 +40,7 @@ test('Find unused class members (isIncludeEntryExports)', async () => {
 
   assert.equal(Object.keys(issues.classMembers['members.ts']).length, 6);
   assert(issues.classMembers['iterator-generator.ts']['AbstractClassGen.unimplemented']);
+  assert(issues.classMembers['iterator.ts']['AbstractClass.implemented']);
   assert(issues.classMembers['index.ts']['Parent.unusedMemberInEntry']);
   assert(issues.classMembers['members.ts']['MyClass.bUnusedPublic']);
   assert(issues.classMembers['members.ts']['MyClass.cUnusedProp']);
@@ -49,7 +51,7 @@ test('Find unused class members (isIncludeEntryExports)', async () => {
 
   assert.deepEqual(counters, {
     ...baseCounters,
-    classMembers: 8,
+    classMembers: 9,
     processed: 5,
     total: 5,
   });
