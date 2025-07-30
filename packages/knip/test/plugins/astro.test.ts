efac577948ae8759fb20920991db77e6de6a4367
Ivo-Evans <52671879+Ivo-Evans@users.noreply.github.com>
2025-07-30 07:41:04 +0100

    Astro: don't interpret files and folders beginning with underscores as entrypoints (#1187)
    Co-authored-by: Ivo Evans <ivo.evansstorrie@rvu.co.uk>

--- packages/knip/test/plugins/astro.test.ts
+++ packages/knip/test/plugins/astro.test.ts
@@ -4,18 +4,30 @@ import { main } from '../../src/index.js';
 import { resolve } from '../../src/util/path.js';
 import baseArguments from '../helpers/baseArguments.js';
 import baseCounters from '../helpers/baseCounters.js';
+import { join } from '../../src/util/path.js';
 
 const cwd = resolve('fixtures/plugins/astro');
 
 test('Find dependencies with the Astro plugin', async () => {
-  const { counters } = await main({
+  const { issues, counters } = await main({
     ...baseArguments,
     cwd,
   });
 
+  assert(issues.exports['src/consts.ts']['UNUSED']);
+
+  assert(issues.files.has(join(cwd, 'src/pages/_top-level-file-unused.ts')));
+  assert(issues.files.has(join(cwd, 'src/pages/_top-level-dir-unused/index.ts')));
+
+  assert(issues.files.has(join(cwd, 'src/pages/blog/_nested-unused-file.ts')));
+  assert(issues.files.has(join(cwd, 'src/pages/blog/_util/unused-component.astro')));
+  assert(issues.files.has(join(cwd, 'src/pages/blog/_util/nested/deeply-nested-unused-file.ts')));
+
   assert.deepEqual(counters, {
     ...baseCounters,
-    processed: 21,
-    total: 21,
+    exports: 1,
+    files: 5,
+    processed: 26,
+    total: 26,
   });
 });
