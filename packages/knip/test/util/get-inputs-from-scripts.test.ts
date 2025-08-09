1ddb0966eef7babb29d3ecc040cebf7d84e854b2
Lars Kappert <lars@webpro.nl>
2025-08-09 09:49:36 +0200

    Binaries don't contain asterisks (e.g. script name wildcard)

--- packages/knip/test/util/get-inputs-from-scripts.test.ts
+++ packages/knip/test/util/get-inputs-from-scripts.test.ts
@@ -245,6 +245,8 @@ test('getInputsFromScripts (nodemon)', () => {
 
 test('getInputsFromScripts (concurrently)', () => {
   t('concurrently "tsx watch s.ts" "tsx watch c.ts"', [toBinary('concurrently'), toBinary('tsx'), toDeferResolveEntry('s.ts', opt), toBinary('tsx'), toDeferResolveEntry('c.ts', opt)]);
+  t('sleep 2 && concurrently "npm:watch-*"', [toBinary('sleep'), toBinary('concurrently')]);
+  t('concurrently -g -c red,green,yellow,blue,magenta pnpm:lint:*', [toBinary('concurrently')]);
 });
 
 test('getInputsFromScripts (double-dash)', () => {
