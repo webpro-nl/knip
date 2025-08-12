cb6c81105a32edc2e001af6697df754e6bd9224f
Girges Scandar <girges.scandar@getyourguide.com>
2025-08-12 02:12:49 +0200

    update formatting

--- packages/knip/test/util/path.test.ts
+++ packages/knip/test/util/path.test.ts
@@ -1,11 +1,11 @@
-import { test } from "bun:test";
-import assert from "node:assert/strict";
-import { cwd, resolve } from "../../src/util/path.js";
+import { test } from 'bun:test';
+import assert from 'node:assert/strict';
+import { cwd, resolve } from '../../src/util/path.js';
 
-test("Should resolve relative paths", () => {
-  assert.equal(resolve("./proc.js"), `${cwd}/proc.js`);
+test('Should resolve relative paths', () => {
+  assert.equal(resolve('./proc.js'), `${cwd}/proc.js`);
 });
 
-test("Should resolve absolute paths", () => {
-  assert.equal(resolve("/proc.js"), "/proc.js");
+test('Should resolve absolute paths', () => {
+  assert.equal(resolve('/proc.js'), '/proc.js');
 });
