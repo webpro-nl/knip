49b9467c8bc9a8d007dd5f22043bcf3534ba05f6
Ivan Tanev <van.tanev@gmail.com>
2025-08-18 18:36:54 +0300

    Fix #1224

--- packages/knip/src/plugins/preconstruct/index.ts
+++ packages/knip/src/plugins/preconstruct/index.ts
@@ -1,6 +1,7 @@
 import type { IsPluginEnabled, Plugin, ResolveConfig } from '../../types/config.js';
 import { toProductionEntry } from '../../util/input.js';
 import { hasDependency } from '../../util/plugin.js';
+import { join } from '../../util/path.js';
 import type { PreconstructConfig } from './types.js';
 
 // https://preconstruct.tools/configuration
@@ -14,7 +15,7 @@ const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependenc
 const config = ['package.json'];
 
 const resolveConfig: ResolveConfig<PreconstructConfig> = async config => {
-  return (config.entrypoints ?? []).map(id => toProductionEntry(id, { allowIncludeExports: true }));
+  return (config.entrypoints ?? []).map(id => toProductionEntry(join("src", id), { allowIncludeExports: true }));
 };
 
 export default {
