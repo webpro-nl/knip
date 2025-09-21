6e339cadc079b0e144c2036134154c696e8b31c0
Jannis Morgenstern <49486580+nyarthan@users.noreply.github.com>
2025-09-21 07:51:03 +0000

    fix: enable pnpm plugin on root config & lockfile (#1275)

--- packages/knip/src/plugins/pnpm/index.ts
+++ packages/knip/src/plugins/pnpm/index.ts
@@ -1,10 +1,15 @@
 import type { IsPluginEnabled, Plugin } from '../../types/config.js';
+import { _firstGlob } from '../../util/glob.js';
 
 // https://pnpm.io/pnpmfile
 
 const title = 'pnpm';
 
-const isEnabled: IsPluginEnabled = ({ manifest }) => Boolean(manifest.packageManager?.startsWith('pnpm@'));
+const isEnabled: IsPluginEnabled = async ({ cwd, manifest }) =>
+  Boolean(
+    manifest.packageManager?.startsWith('pnpm@') ||
+      (await _firstGlob({ cwd, patterns: ['pnpm-lock.yaml', 'pnpm-workspace.yaml'] }))
+  );
 
 const config: string[] = ['.pnpmfile.cjs'];
 
