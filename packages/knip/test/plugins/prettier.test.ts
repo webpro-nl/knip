5dd115f29d073916c3f612334758197a94c18621
rfalke-rtl <144100445+rfalke-rtl@users.noreply.github.com>
2025-10-09 14:53:36 +0200

    Add support for json5 files (#1290)

--- packages/knip/test/plugins/prettier.test.ts
+++ packages/knip/test/plugins/prettier.test.ts
@@ -24,3 +24,20 @@ test('Find dependencies with the Prettier plugin', async () => {
     total: 0,
   });
 });
+
+test('Find dependencies with the Prettier plugin (.json5 config)', async () => {
+  const cwd = resolve('fixtures/plugins/prettier-json5');
+  const options = await createOptions({ cwd });
+  const { issues, counters } = await main(options);
+
+  assert(issues.devDependencies['package.json']['prettier']);
+  assert(issues.unlisted['.prettierrc.json5']['prettier-plugin-tailwindcss']);
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    devDependencies: 1,
+    unlisted: 1,
+    processed: 0,
+    total: 0,
+  });
+});
