87ca476cdc1ebcc7637e2ff17a88e4fd7dfe790d
Lars Kappert <lars@webpro.nl>
2025-10-13 20:41:02 +0200

    Add coverage for `ignoreFiles` feat

---
+++ packages/knip/test/ignore-files.test.ts
@@ -0,0 +1,27 @@
+import assert from 'node:assert/strict';
+import test from 'node:test';
+import { main } from '../src/index.js';
+import { createOptions } from '../src/util/create-options.js';
+import { join } from '../src/util/path.js';
+import baseCounters from './helpers/baseCounters.js';
+import { resolve } from './helpers/resolve.js';
+
+const cwd = resolve('fixtures/ignore-files');
+
+test('Respect ignored files', async () => {
+  const options = await createOptions({ cwd });
+  const { issues, counters } = await main(options);
+
+  assert(issues.files.has(join(cwd, 'apples/rooted.js')));
+  assert(issues.files.has(join(cwd, 'unused.js')));
+
+  assert(issues.exports['apples/used.js'].unused);
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    files: 2,
+    exports: 1,
+    processed: 8,
+    total: 12,
+  });
+});
