2464f3704a11b0c6d1f71a1850f4fa928e6c623f
Lars Kappert <lars@webpro.nl>
2025-10-20 18:30:25 +0200

    Fix negated patterns from package.json#exports (related to #1308)

--- packages/knip/src/util/to-source-path.ts
+++ packages/knip/src/util/to-source-path.ts
@@ -3,7 +3,7 @@ import type { ConfigurationChief, Workspace } from '../ConfigurationChief.js';
 import { DEFAULT_EXTENSIONS } from '../constants.js';
 import { debugLog, debugLogArray } from './debug.js';
 import { isDirectory } from './fs.js';
-import { _glob, _syncGlob } from './glob.js';
+import { _glob, _syncGlob, prependDirToPattern } from './glob.js';
 import { isAbsolute, isInternal, join, toRelative } from './path.js';
 
 const defaultExtensions = `.{${DEFAULT_EXTENSIONS.map(ext => ext.slice(1)).join(',')}}`;
@@ -46,7 +46,7 @@ export const getToSourcePathsHandler = (chief: ConfigurationChief) => {
     const patterns = new Set<string>();
 
     for (const specifier of specifiers) {
-      const absSpecifier = isAbsolute(specifier) ? specifier : join(dir, specifier);
+      const absSpecifier = isAbsolute(specifier) ? specifier : prependDirToPattern(dir, specifier);
       const ws = chief.findWorkspaceByFilePath(absSpecifier);
       if (ws?.srcDir && ws.outDir && !absSpecifier.startsWith(ws.srcDir) && absSpecifier.startsWith(ws.outDir)) {
         const pattern = absSpecifier.replace(ws.outDir, ws.srcDir).replace(matchExt, extensions);
