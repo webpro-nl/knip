208381009cf99a15c0b1fe3feecbc202cbe4d7a1
Lars Kappert <lars@webpro.nl>
2025-11-07 10:17:02 +0100

    Improve zod error message

--- packages/knip/src/util/errors.ts
+++ packages/knip/src/util/errors.ts
@@ -19,6 +19,15 @@ export const hasErrorCause = (error: Error): error is ErrorWithCause =>
 export const isConfigurationError = (error: Error) => error instanceof ConfigurationError;
 
 export const getKnownErrors = (error: Error) => {
-  if (isZodErrorLike(error)) return [...error.issues].map(error => new Error(error.message));
+  if (isZodErrorLike(error))
+    return [...error.issues].map(error => {
+      let message = error.message;
+      const details = [];
+      if (error.path.length > 0) details.push(`location: ${error.path.join('.')}`);
+      // @ts-expect-error
+      if (typeof error.expected === 'string') details.push(`expected: ${error.expected}`);
+      if (details.length > 0) message += ` (${details.join(', ')})`;
+      return new Error(message);
+    });
   return [error];
 };
