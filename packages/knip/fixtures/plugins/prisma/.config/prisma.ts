6dc700a1351c776342ca54b6968865ed3bc9fd43
Henry Cai <huacong.cai@gmail.com>
2025-11-08 23:37:33 -0800

    feat(plugins): extend prisma plugin (#1339)

--- packages/knip/fixtures/plugins/prisma/.config/prisma.ts
+++ packages/knip/fixtures/plugins/prisma/.config/prisma.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   migrations: {
-    seed: 'tsx prisma/seed2.ts',
+    seed: 'tsx ../prisma/seed3.ts',
   },
 });
--- packages/knip/test/plugins/prisma.test.ts
+++ packages/knip/test/plugins/prisma.test.ts
@@ -13,7 +13,7 @@ test('Find dependencies with the Prisma plugin', async () => {
 
   assert.deepEqual(counters, {
     ...baseCounters,
-    processed: 3,
-    total: 3,
+    processed: 6,
+    total: 6,
   });
 });
