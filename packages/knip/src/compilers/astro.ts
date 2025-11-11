b424d58e54e3da6328393bec8634ad79179c8028
Ivo Evans <ivo.evansstorrie@rvu.co.uk>
2025-11-11 16:27:48 +0000

    Tests pass

--- packages/knip/src/compilers/astro.ts
+++ packages/knip/src/compilers/astro.ts
@@ -14,18 +14,24 @@ const compiler = async (text: string) => {
     }
 
     if (is.tag(parent) && parent.name === "script") {
-      const scriptText = parent?.children
-        .filter((child) => child.type === "text")
-        .map((child) => child.value);
-      detected.push(...scriptText);
+      const scriptSrc = parent?.attributes?.find(attribute => attribute.name === "src")?.value
+      if (scriptSrc) {
+        const isExternalScript = scriptSrc.startsWith("http") || scriptSrc.startsWith("/") // a script starting with `/` is considered external, in the sense that it's fetched over the network at runtime. However Astro does support you putting scripts into a `public` dir and then consuming them at `/`, so you could argue that's an import. The risk of false positives is high, though, since scripts other than those at `public/` might be available at `/`.
+        if (!isExternalScript) {
+          detected.push(`import ${JSON.stringify(scriptSrc)};`)
+        }
+      } else {
+        const scriptText = parent?.children
+          .filter((child) => child.type === "text")
+          .map((child) => child.value);
+        detected.push(...scriptText);
+      }
     } else if ("children" in parent) {
       return parent.children.forEach((child) => traverse(child));
     }
   };
 
-  const parseResult = await parse(text, {
-    position: false, // defaults to `true`
-  });
+  const parseResult = await parse(text);
 
   traverse(parseResult.ast);
 
