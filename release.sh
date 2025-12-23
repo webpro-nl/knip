431d530a65a228ae550efbcb83ac0065b71b94b5
Lars Kappert <lars@webpro.nl>
2025-12-23 22:20:28 +0100

    Complete the release flow

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@ set -e
 pnpm run --dir packages/knip release
 pnpm run --dir packages/language-server release --no-git.changelog --no-git.push
 pnpm run --dir packages/mcp-server release --no-git.changelog --no-git.push
-pnpm run --dir packages/vscode-knip publish --no-git.changelog
+pnpm run --dir packages/vscode-knip release --no-git.changelog
