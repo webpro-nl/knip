e4775735d48c0e5561c0015c7564372d42cffdcd
Alexander Karan <tech@alexanderkaran.com>
2025-12-24 06:52:37 +1100

    Cleaned up Vitepress example

--- packages/knip/fixtures/plugins/vitepress/.vitepress/config.mts
+++ packages/knip/fixtures/plugins/vitepress/.vitepress/config.mts
@@ -8,21 +8,6 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
     ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
   }
 })
--- packages/knip/src/plugins/vitepress/index.ts
+++ packages/knip/src/plugins/vitepress/index.ts
@@ -11,28 +11,16 @@ const enablers = ['vitepress'];
 
 const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependencies, enablers);
 
-const config: string[] = [];
-
 const entry: string[] = [
   ".vitepress/config.{js,ts,mjs,mts}",
   ".vitepress/theme/index.{js,ts,mjs,mts}"
 ];
 
-const production: string[] = [];
-
-const resolveConfig: ResolveConfig<PluginConfig> = async config => {
-  const inputs = config?.plugins ?? [];
-  return [...inputs].map(id => toDeferResolve(id));
-};
-
 const plugin: Plugin = {
   title,
   enablers,
   isEnabled,
-  config,
   entry,
-  production,
-  resolveConfig,
 };
 
 export default plugin;
