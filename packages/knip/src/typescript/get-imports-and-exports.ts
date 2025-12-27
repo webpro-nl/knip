2ec95f762a01a0b7ec09837c8823373cc81a27cc
Lars Kappert <lars@webpro.nl>
2025-12-27 09:37:15 +0100

    Consider exported type used in exported interface used (resolve #1250)

--- packages/knip/src/typescript/get-imports-and-exports.ts
+++ packages/knip/src/typescript/get-imports-and-exports.ts
@@ -529,7 +529,7 @@ const getImportsAndExports = (
   // and if it's referenced in an exported type and therefore should be exported with it (*)
   for (const item of exports.values()) {
     // TODO Reconsider this messy logic in AST visitors + `isReferencedInExport` + `findInternalReferences`
-    if (!isType(item) && item.symbol && referencedSymbolsInExport.has(item.symbol)) {
+    if (item.symbol && referencedSymbolsInExport.has(item.symbol)) {
       item.self = [1, true];
     } else {
       const isBindingElement = item.symbol?.valueDeclaration && ts.isBindingElement(item.symbol.valueDeclaration);
