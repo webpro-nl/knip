af536ca6c3a732be8f59824d7d4a09840dd9aa1e
Lars Kappert <lars@webpro.nl>
2025-12-27 09:37:15 +0100

    Improve/loosen up source mapping (resolve #1256)

---
+++ packages/knip/fixtures/subpath-imports-outdir/src/test/test1.ts
@@ -0,0 +1,5 @@
+import { test2 } from '#src/test/test2.js';
+
+export const test1 = () => {
+  return test2();
+};
--- packages/knip/src/util/to-source-path.ts
+++ packages/knip/src/util/to-source-path.ts
@@ -8,12 +8,12 @@ import { isAbsolute, isInternal, join, toRelative } from './path.js';
 
 const defaultExtensions = `.{${DEFAULT_EXTENSIONS.map(ext => ext.slice(1)).join(',')}}`;
 const hasTSExt = /(?<!\.d)\.(m|c)?tsx?$/;
-const hasDTSExt = /.d\.(m|c)?ts$/;
 const matchExt = /(\.d)?\.(m|c)?(j|t)s$/;
 
 export const augmentWorkspace = (workspace: Workspace, dir: string, compilerOptions: CompilerOptions) => {
   const srcDir = join(dir, 'src');
-  workspace.srcDir = compilerOptions.rootDir ?? (isDirectory(srcDir) ? srcDir : dir);
+  const outDirHasSrc = compilerOptions.outDir && isDirectory(join(compilerOptions.outDir, 'src'));
+  workspace.srcDir = compilerOptions.rootDir ?? (outDirHasSrc ? dir : isDirectory(srcDir) ? srcDir : dir);
   workspace.outDir = compilerOptions.outDir || workspace.srcDir;
 };
 
@@ -25,10 +25,7 @@ export const getModuleSourcePathHandler = (chief: ConfigurationChief) => {
     if (toSourceMapCache.has(filePath)) return toSourceMapCache.get(filePath);
     const workspace = chief.findWorkspaceByFilePath(filePath);
     if (workspace?.srcDir && workspace.outDir) {
-      if (
-        (!filePath.startsWith(workspace.srcDir) && filePath.startsWith(workspace.outDir)) ||
-        (workspace.srcDir === workspace.outDir && hasDTSExt.test(filePath))
-      ) {
+      if (filePath.startsWith(workspace.outDir) || workspace.srcDir === workspace.outDir) {
         const pattern = filePath.replace(workspace.outDir, workspace.srcDir).replace(matchExt, defaultExtensions);
         const srcFilePath = _syncGlob({ patterns: pattern })[0];
         toSourceMapCache.set(filePath, srcFilePath);
