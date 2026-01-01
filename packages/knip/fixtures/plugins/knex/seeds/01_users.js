af0ebaa98537be9c95f32c2c3b58cb506922d9a3
Alexander Karan <47707063+AlexanderKaran@users.noreply.github.com>
2026-01-01 19:58:28 +0800

    1289 knex plugin (#1418)
    Co-authored-by: Lars Kappert <lars@webpro.nl>

---
+++ packages/knip/fixtures/plugins/knex/seeds/01_users.js
@@ -0,0 +1,8 @@
+exports.seed = async function (knex) {
+  await knex('users').del();
+  await knex('users').insert([
+    { id: 1, email: 'user1@example.com', name: 'User One' },
+    { id: 2, email: 'user2@example.com', name: 'User Two' },
+    { id: 3, email: 'user3@example.com', name: 'User Three' },
+  ]);
+};
