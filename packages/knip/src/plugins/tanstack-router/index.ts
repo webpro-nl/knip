5fc138e1a3946c7aedc363524ae67cbcadd240a9
Alexander Karan <tech@alexanderkaran.com>
2026-01-07 17:46:40 +0800

    Add routes glob to production entries with config support

--- packages/knip/src/plugins/tanstack-router/index.ts
+++ packages/knip/src/plugins/tanstack-router/index.ts
@@ -1,5 +1,8 @@
-import type { IsPluginEnabled, Plugin } from '../../types/config.js';
+import type { IsPluginEnabled, Plugin, ResolveConfig } from '../../types/config.js';
+import { toProductionEntry } from '../../util/input.js';
+import { join, toAbsolute } from '../../util/path.js';
 import { hasDependency } from '../../util/plugin.js';
+import type { TanStackRouterConfig } from './types.js';
 
 // https://tanstack.com/router/latest/docs/framework/react/routing/file-based-routing
 
@@ -16,13 +19,32 @@ const enablers = [
 
 const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependencies, enablers);
 
-const entry = ['src/routeTree.gen.{ts,js}'];
+const config = ['tsr.config.json'];
+
+const production = ['src/routeTree.gen.{ts,js}', 'src/routes/**/*.{ts,tsx,js,jsx}'];
+
+const resolveConfig: ResolveConfig<TanStackRouterConfig> = async (localConfig, options) => {
+  const { configFileDir } = options;
+
+  const routesDirectory = localConfig.routesDirectory ?? './src/routes';
+  const generatedRouteTree = localConfig.generatedRouteTree ?? './src/routeTree.gen.ts';
+
+  const routesDir = toAbsolute(routesDirectory, configFileDir);
+  const routeTreePath = toAbsolute(generatedRouteTree, configFileDir);
+
+  return [
+    toProductionEntry(routeTreePath, { allowIncludeExports: true }),
+    toProductionEntry(join(routesDir, '**/*.{ts,tsx,js,jsx}'), { allowIncludeExports: true }),
+  ];
+};
 
 const plugin: Plugin = {
   title,
   enablers,
   isEnabled,
-  entry,
+  config,
+  production,
+  resolveConfig,
 };
 
 export default plugin;
