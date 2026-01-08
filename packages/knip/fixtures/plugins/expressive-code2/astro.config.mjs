2514f27d55311bd2c17729ab1693c03280c0dd04
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2026-01-08 21:42:22 +0900

    feat: add plugin for expressive-code
    Co-authored-by: Cyle <cyle@cylewaitfor.it>

---
+++ packages/knip/fixtures/plugins/expressive-code2/astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import expressiveCode from 'astro-expressive-code';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [expressiveCode(), mdx(), sitemap()],
+});
