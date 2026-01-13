ecf5e34b51f3817dabcf50ca6705141aea3f56e5
Lynn Romich <lynn@lynn.zone>
2026-01-13 18:27:14 +0100

    Add Nitro plugin (#1415)

--- packages/knip/src/plugins/index.ts
+++ packages/knip/src/plugins/index.ts
@@ -58,6 +58,7 @@ import { default as netlify } from './netlify/index.js';
 import { default as next } from './next/index.js';
 import { default as nextIntl } from './next-intl/index.js';
 import { default as nextMdx } from './next-mdx/index.js';
+import { default as nitro } from './nitro/index.js';
 import { default as node } from './node/index.js';
 import { default as nodeModulesInspector } from './node-modules-inspector/index.js';
 import { default as nodemon } from './nodemon/index.js';
@@ -188,6 +189,7 @@ export const Plugins = {
   next,
   'next-intl': nextIntl,
   'next-mdx': nextMdx,
+  nitro,
   node,
   'node-modules-inspector': nodeModulesInspector,
   nodemon,
