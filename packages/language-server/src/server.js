61e7a24460e11bd2e9e27e9a791953eb004947df
Lars Kappert <lars@webpro.nl>
2026-01-30 08:23:02 +0100

    Lint/group import statements

--- packages/language-server/src/server.js
+++ packages/language-server/src/server.js
@@ -3,9 +3,6 @@ import path from 'node:path';
 import { fileURLToPath, pathToFileURL } from 'node:url';
 import { createOptions, createSession, KNIP_CONFIG_LOCATIONS } from 'knip/session';
 import { FileChangeType, ProposedFeatures, TextDocuments } from 'vscode-languageserver';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8'));
 import { CodeActionKind, createConnection } from 'vscode-languageserver/node.js';
 import { TextDocument } from 'vscode-languageserver-textdocument';
 import {
@@ -26,6 +23,9 @@ import {
   SESSION_LOADING,
 } from './constants.js';
 import { issueToDiagnostic } from './diagnostics.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8'));
 
 const RESTART_FOR = new Set(['package.json', ...KNIP_CONFIG_LOCATIONS]);
 
