bdff7c6e7bb335bb428a90e8b18e32ac6ff72339
Lars Kappert <lars@webpro.nl>
2026-02-05 21:40:17 +0100

    Use --stdio if no transport provided

--- packages/language-server/src/index.js
+++ packages/language-server/src/index.js
@@ -1,3 +1,6 @@
 import { LanguageServer } from './server.js';
 
+const transports = ['--stdio', '--socket', '--node-ipc', '--pipe'];
+if (!process.argv.some(arg => transports.includes(arg))) process.argv.push('--stdio');
+
 new LanguageServer();
