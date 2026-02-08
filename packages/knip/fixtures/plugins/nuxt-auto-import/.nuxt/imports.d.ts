3283d11e428fdfc771edaec9ff2dc2c4e5c20e52
Lars Kappert <lars@webpro.nl>
2026-02-08 07:29:27 +0100

    Support auto-imports in Nuxt plugin

---
+++ packages/knip/fixtures/plugins/nuxt-auto-import/.nuxt/imports.d.ts
@@ -0,0 +1,4 @@
+export { ref, computed } from 'vue';
+export { useCounter } from '../composables/useCounter';
+export { useTheme } from '../composables/useTheme';
+export { formatDate } from '../utils/format';
