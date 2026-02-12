2d8c506aebb74beedebc19ce6a0c452b5815f2de
Julien Goux <hi@jgoux.dev>
2026-02-12 22:14:04 +0100

    add support for bun test preload files

--- packages/knip/src/plugins/bun/index.ts
+++ packages/knip/src/plugins/bun/index.ts
@@ -1,6 +1,7 @@
 import parseArgs from 'minimist';
-import type { IsPluginEnabled, Plugin, Resolve } from '../../types/config.js';
-import { toEntry } from '../../util/input.js';
+import type { IsPluginEnabled, Plugin, ResolveConfig, Resolve } from '../../types/config.js';
+import { toDeferResolve, toEntry } from '../../util/input.js';
+import type { BunfigConfig } from './types.js';
 
 // https://bun.sh/docs/cli/test
 
@@ -13,7 +14,14 @@ const hasBunTest = (scripts: Record<string, string> | undefined) =>
 
 const isEnabled: IsPluginEnabled = ({ manifest }) => !!hasBunTest(manifest.scripts);
 
+const config = ['bunfig.toml'];
+
 const patterns = ['**/*.{test,spec}.{js,jsx,ts,tsx}', '**/*_{test,spec}.{js,jsx,ts,tsx}'];
+
+const resolveConfig: ResolveConfig<BunfigConfig> = localConfig => {
+  const preload = localConfig.test?.preload ?? [];
+  return preload.map(specifier => toDeferResolve(specifier));
+};
 
 const resolve: Resolve = options => {
   const scripts = { ...options.rootManifest?.scripts, ...options.manifest.scripts };
@@ -21,7 +29,9 @@ const resolve: Resolve = options => {
     if (/(?<=^|\s)bun test/.test(script)) {
       const parsed = parseArgs(script.split(' '), { string: ['timeout', 'rerun-each', 'preload'] });
       const args = parsed._.filter(id => id !== 'bun' && id !== 'test');
-      return (args.length === 0 ? patterns : args).map(toEntry);
+      const inputs = (args.length === 0 ? patterns : args).map(toEntry);
+      if (parsed.preload) inputs.push(toDeferResolve(parsed.preload));
+      return inputs;
     }
   }
   return [];
@@ -31,7 +41,9 @@ const plugin: Plugin = {
   title,
   enablers,
   isEnabled,
+  config,
   resolve,
+  resolveConfig,
 };
 
 export default plugin;
