2e1ec590b3edf4364cceeabd92783d3ab1e8b640
Wojciech Maj <kontakt@wojtekmaj.pl>
2026-02-17 13:57:59 +0100

    Add unit tests for ecosystem.config.*

--- packages/knip/test/plugins/pm2.test.ts
+++ packages/knip/test/plugins/pm2.test.ts
@@ -7,6 +7,7 @@ import { createOptions } from '../helpers/create-options.js';
 import { resolve } from '../helpers/resolve.js';
 
 const cwd = resolve('fixtures/plugins/pm2');
+const cwdEcosystem = resolve('fixtures/plugins/pm2-ecosystem');
 
 test('Find entry files with the pm2 plugin', async () => {
   const options = await createOptions({ cwd });
@@ -14,6 +15,21 @@ test('Find entry files with the pm2 plugin', async () => {
 
   assert(!issues.files.has(join(cwd, 'src/another.js')));
   assert(issues.files.has(join(cwd, 'src/unused.js')));
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    files: 1,
+    processed: 3,
+    total: 3,
+  });
+});
+
+test('Find entry files with the pm2 plugin from ecosystem config', async () => {
+  const options = await createOptions({ cwd: cwdEcosystem });
+  const { issues, counters } = await main(options);
+
+  assert(!issues.files.has(join(cwdEcosystem, 'src/another.js')));
+  assert(issues.files.has(join(cwdEcosystem, 'src/unused.js')));
 
   assert.deepEqual(counters, {
     ...baseCounters,
