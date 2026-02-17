c81e8435a6a4e97f49b8db8419dee6530d5ba79b
Wojciech Maj <kontakt@wojtekmaj.pl>
2026-02-17 13:54:12 +0100

    Remove pm2-global

--- packages/knip/src/plugins/pm2/index.ts
+++ packages/knip/src/plugins/pm2/index.ts
@@ -1,5 +1,4 @@
 import type { IsPluginEnabled, Plugin, ResolveConfig } from '../../types/config.js';
-import { isFile } from '../../util/fs.js';
 import { toEntry } from '../../util/input.js';
 import { isInternal } from '../../util/path.js';
 import { hasDependency } from '../../util/plugin.js';
@@ -9,19 +8,7 @@ const title = 'pm2';
 
 const enablers = ['pm2'];
 
-const configFileNames = [
-  'pm2.config.json',
-  'pm2.config.js',
-  'pm2.config.cjs',
-  'pm2.config.mjs',
-  'ecosystem.config.json',
-  'ecosystem.config.js',
-  'ecosystem.config.cjs',
-  'ecosystem.config.mjs',
-];
-
-const isEnabled: IsPluginEnabled = ({ cwd, dependencies }) =>
-  hasDependency(dependencies, enablers) || configFileNames.some(name => isFile(cwd, name));
+const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependencies, enablers);
 
 const config = ['pm2.config.{json,js,cjs,mjs}', 'ecosystem.config.{json,js,cjs,mjs}'];
 
--- packages/knip/test/plugins/pm2.test.ts
+++ packages/knip/test/plugins/pm2.test.ts
@@ -7,7 +7,6 @@ import { createOptions } from '../helpers/create-options.js';
 import { resolve } from '../helpers/resolve.js';
 
 const cwd = resolve('fixtures/plugins/pm2');
-const cwdGlobal = resolve('fixtures/plugins/pm2-global');
 
 test('Find entry files with the pm2 plugin', async () => {
   const options = await createOptions({ cwd });
@@ -15,21 +14,6 @@ test('Find entry files with the pm2 plugin', async () => {
 
   assert(!issues.files.has(join(cwd, 'src/another.js')));
   assert(issues.files.has(join(cwd, 'src/unused.js')));
-
-  assert.deepEqual(counters, {
-    ...baseCounters,
-    files: 1,
-    processed: 3,
-    total: 3,
-  });
-});
-
-test('Find entry files with the pm2 plugin without pm2 in dependencies', async () => {
-  const options = await createOptions({ cwd: cwdGlobal });
-  const { issues, counters } = await main(options);
-
-  assert(!issues.files.has(join(cwdGlobal, 'src/another.js')));
-  assert(issues.files.has(join(cwdGlobal, 'src/unused.js')));
 
   assert.deepEqual(counters, {
     ...baseCounters,
