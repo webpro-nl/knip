c8e7472bb8753453893ca3b9b8f62c36974612aa
roboin <50550561+Robot-Inventor@users.noreply.github.com>
2026-02-19 19:17:20 +0900

    test: add `buildConfig` function

--- packages/knip/fixtures/plugins/payload/payload.config.ts
+++ packages/knip/fixtures/plugins/payload/payload.config.ts
@@ -1,9 +1,11 @@
 import type { Config } from 'payload';
 
-export default {
+const buildConfig = async (config: Config) => config;
+
+export default buildConfig({
   admin: {
     importMap: {
       importMapFile: 'src/app/(payload)/importMap.js',
     },
   },
-} satisfies Config;
+});
