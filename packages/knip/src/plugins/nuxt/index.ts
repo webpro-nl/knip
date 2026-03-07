56577e680e886211b88adc93bfcdbfc0ca23309d
Lars Kappert <lars@webpro.nl>
2026-03-07 08:08:23 +0100

    Add global component glob to production entries

--- packages/knip/src/plugins/nuxt/index.ts
+++ packages/knip/src/plugins/nuxt/index.ts
@@ -77,6 +77,7 @@ const addAppEntries = (inputs: Input[], srcDir: string, serverDir: string, confi
   inputs.push(toProductionEntry(join(srcDir, middleware(config.dir?.middleware))));
   inputs.push(toProductionEntry(join(srcDir, pages(config.dir?.pages))));
   inputs.push(toProductionEntry(join(srcDir, plugins(config.dir?.plugins))));
+  inputs.push(toProductionEntry(join(srcDir, 'components/global/**/*.{vue,jsx,tsx}')));
   for (const id of server) inputs.push(toProductionEntry(join(dir, serverDir, id)));
   inputs.push(toProductionEntry(join(dir, modules)));
   if (config.css)
