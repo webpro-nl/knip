09bbce539229c5666a313ebf1a120dd885cae6e8
Lars Kappert <lars@webpro.nl>
2026-03-22 18:53:37 +0100

    Bump get-tsconfig to 4.13.7, remove patch (resolve #1630)

---
+++ packages/knip/test/tsconfig-files-extends.test.ts
@@ -0,0 +1,21 @@
+import assert from 'node:assert/strict';
+import test from 'node:test';
+import { main } from '../src/index.ts';
+import baseCounters from './helpers/baseCounters.ts';
+import { createOptions } from './helpers/create-options.ts';
+import { resolve } from './helpers/resolve.ts';
+
+const cwd = resolve('fixtures/tsconfig-files-extends');
+
+test('Preserve tsconfig files from extended config when include is set', async () => {
+  const options = await createOptions({ cwd });
+  const { issues, counters } = await main(options);
+
+  assert.equal(Object.keys(issues.files).length, 0);
+
+  assert.deepEqual(counters, {
+    ...baseCounters,
+    processed: 3,
+    total: 3,
+  });
+});
