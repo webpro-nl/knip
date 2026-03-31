1d5f9438e2e4e0d01558f41608a450093afa60e5
Lars Kappert <lars@webpro.nl>
2026-03-31 08:56:57 +0200

    Fix `reference types` regex to allow extra attributes (resolve #1649)

--- packages/knip/src/typescript/get-imports-and-exports.ts
+++ packages/knip/src/typescript/get-imports-and-exports.ts
@@ -24,7 +24,7 @@ import { walkAST } from './visitors/walk.ts';
 const jsDocImportRe = /import\(\s*['"]([^'"]+)['"]\s*\)(?:\.(\w+))?/g;
 const jsDocImportTagRe = /@import\s+(?:\{[^}]*\}|\*\s+as\s+\w+)\s+from\s+['"]([^'"]+)['"]/g;
 const jsxImportSourceRe = /@jsxImportSource\s+(\S+)/;
-const referenceTypesRe = /\s*<reference\s+types\s*=\s*"([^"]+)"\s*\/>/;
+const referenceTypesRe = /\s*<reference\s+types\s*=\s*"([^"]+)"[^/]*\/>/;
 const envRe = /@(?:vitest|jest)-environment\s+(\S+)/g;
 
 interface AddInternalImportOptions {
