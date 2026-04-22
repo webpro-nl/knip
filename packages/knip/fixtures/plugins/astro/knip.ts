46cb33871b236249cf9b09679988d530a61a545f
Lars Kappert <lars@webpro.nl>
2026-04-22 11:04:19 +0200

    Treat Props as used when Astro.props is referenced (resolve #1629)

--- packages/knip/fixtures/plugins/astro/knip.ts
+++ packages/knip/fixtures/plugins/astro/knip.ts
@@ -1,5 +1,6 @@
 export default {
   ignore: '.astro/types.d.ts',
+  ignoreExportsUsedInFile: true,
   compilers: {
     css: (text: string) => [...text.matchAll(/(?<=@)import[^;]+/g)].join('\n'),
   },
