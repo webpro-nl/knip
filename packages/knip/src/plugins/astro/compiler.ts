0bd869f7bf980c77dee78b9a693a87f44f22ce3e
Lars Kappert <lars@webpro.nl>
2026-04-22 11:04:19 +0200

    Treat Props as used when Astro.props is referenced (resolve #1629)

--- packages/knip/src/plugins/astro/compiler.ts
+++ packages/knip/src/plugins/astro/compiler.ts
@@ -1,10 +1,16 @@
 import { frontmatterMatcher, scriptBodies } from '../../compilers/compilers.ts';
 
+const propsDeclMatcher = /(?:^|[\s;])(?:interface|type)\s+Props\b/;
+
 const compiler = (text: string, path: string) => {
   const scripts = [];
 
   const frontmatter = text.match(frontmatterMatcher);
-  if (frontmatter?.[1]) scripts.push(frontmatter[1]);
+  if (frontmatter?.[1]) {
+    let fm = frontmatter[1];
+    if (propsDeclMatcher.test(fm) && text.includes('Astro.props')) fm += '\ntype __knip_astro_props = Props;';
+    scripts.push(fm);
+  }
 
   const scriptContent = scriptBodies(text, path);
   if (scriptContent) scripts.push(scriptContent);
