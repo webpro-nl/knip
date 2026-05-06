7cc4fc19ea12f4aefb55ef01a0ad5237b2dac8c4
Lars Kappert <lars@webpro.nl>
2026-05-06 11:18:57 +0200

    Resolve Cypress reporter set per testing type (resolve #1724)

--- packages/knip/fixtures/plugins/cypress2/cypress.config.js
+++ packages/knip/fixtures/plugins/cypress2/cypress.config.js
@@ -3,8 +3,10 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     specPattern: '**/*.e2e.js',
+    reporter: 'mochawesome',
   },
   component: {
     specPattern: ['**/*.component.js'],
+    reporter: 'mocha-junit-reporter',
   },
 });
