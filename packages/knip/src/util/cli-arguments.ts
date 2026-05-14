6f12997b1c08d9782820dc334958d94a28b00928
Lars Kappert <lars@webpro.nl>
2026-05-14 20:06:45 +0200

    Align --help text

--- packages/knip/src/util/cli-arguments.ts
+++ packages/knip/src/util/cli-arguments.ts
@@ -5,61 +5,61 @@ export const helpText = `✂️  Find unused dependencies, exports and files in 
 Usage: knip [options]
 
 Options:
-  -h, --help               Print this help text
-  -V, --version            Print version
-  -n, --no-progress        Don't show dynamic progress updates (automatically enabled in CI environments)
-  -c, --config [file]      Configuration file path
-                           (default: [.]knip.json[c], knip.(js|ts), knip.config.(js|ts) or package.json#knip)
-  --use-tsconfig-files     Use tsconfig.json to define project files (override \`project\` patterns)
-  -t, --tsConfig [file]    TypeScript configuration path (default: tsconfig.json)
+  -h, --help                   Print this help text
+  -V, --version                Print version
+  -n, --no-progress            Don't show dynamic progress updates (automatically enabled in CI environments)
+  -c, --config [file]          Configuration file path
+                               (default: [.]knip.json[c], knip.(js|ts), knip.config.(js|ts) or package.json#knip)
+      --use-tsconfig-files     Use tsconfig.json to define project files (override \`project\` patterns)
+  -t, --tsConfig [file]        TypeScript configuration path (default: tsconfig.json)
 
 Mode
-  --cache                  Enable caching
-  --cache-location         Change cache location (default: node_modules/.cache/knip)
-  --include-entry-exports  Include entry files when reporting unused exports
-  --no-gitignore           Don't respect .gitignore
-  --production             Analyze only production source files (e.g. no test files, devDependencies)
-  --strict                 Consider only direct dependencies of workspace (not devDependencies, not other workspaces)
-  --watch                  Watch mode
+      --cache                  Enable caching
+      --cache-location         Change cache location (default: node_modules/.cache/knip)
+      --include-entry-exports  Include entry files when reporting unused exports
+      --no-gitignore           Don't respect .gitignore
+      --production             Analyze only production source files (e.g. no test files, devDependencies)
+      --strict                 Consider only direct dependencies of workspace (not devDependencies, not other workspaces)
+      --watch                  Watch mode
 
 Scope
-  -W, --workspace [filter] Filter workspaces by name, directory, or glob (can be repeated)
-  --directory [dir]        Run process from a different directory (default: cwd)
-  --include                Report only provided issue type(s), can be comma-separated or repeated (1)
-  --exclude                Exclude provided issue type(s) from report, can be comma-separated or repeated (1)
-  --dependencies           Shortcut for --include dependencies,unlisted,binaries,unresolved,catalog
-  --exports                Shortcut for --include exports,nsExports,types,nsTypes,enumMembers,namespaceMembers,duplicates
-  --files                  Shortcut for --include files
-  --tags                   Include or exclude tagged exports
+  -W, --workspace [filter]     Filter workspaces by name, directory, or glob (can be repeated)
+      --directory [dir]        Run process from a different directory (default: cwd)
+      --include                Report only provided issue type(s), can be comma-separated or repeated (1)
+      --exclude                Exclude provided issue type(s) from report, can be comma-separated or repeated (1)
+      --dependencies           Shortcut for --include dependencies,unlisted,binaries,unresolved,catalog
+      --exports                Shortcut for --include exports,nsExports,types,nsTypes,enumMembers,namespaceMembers,duplicates
+      --files                  Shortcut for --include files
+      --tags                   Include or exclude tagged exports
 
 Fix
-  --fix                    Fix issues (modifies files in your repo)
-  --fix-type               Fix only issues of type, can be comma-separated or repeated (2)
-  --allow-remove-files     Allow Knip to remove files (with --fix)
-  --format                 Format modified files after --fix using the local formatter
+      --fix                    Fix issues (modifies files in your repo)
+      --fix-type               Fix only issues of type, can be comma-separated or repeated (2)
+      --allow-remove-files     Allow Knip to remove files (with --fix)
+      --format                 Format modified files after --fix using the local formatter
 
 Output
-  --preprocessor           Preprocess the results before providing it to the reporter(s), can be repeated
-  --preprocessor-options   Pass extra options to the preprocessor (as JSON string, see --reporter-options example)
-  --reporter               Select reporter (default: symbols), can be repeated (3)
-  --reporter-options       Pass extra options to the reporter (as JSON string, see example)
-  --no-config-hints        Suppress configuration hints
-  --treat-config-hints-as-errors    Exit with non-zero code (1) if there are any configuration hints
-  --max-issues             Maximum number of total issues before non-zero exit code (default: 0)
-  --max-show-issues        Maximum number of issues to display per type
-  --no-exit-code           Always exit with code zero (0)
+      --preprocessor           Preprocess the results before providing it to the reporter(s), can be repeated
+      --preprocessor-options   Pass extra options to the preprocessor (as JSON string, see --reporter-options example)
+      --reporter               Select reporter (default: symbols), can be repeated (3)
+      --reporter-options       Pass extra options to the reporter (as JSON string, see example)
+      --no-config-hints        Suppress configuration hints
+      --treat-config-hints-as-errors   Exit with non-zero code (1) if there are any configuration hints
+      --max-issues             Maximum number of total issues before non-zero exit code (default: 0)
+      --max-show-issues        Maximum number of issues to display per type
+      --no-exit-code           Always exit with code zero (0)
 
 Troubleshooting
-  -d, --debug              Show debug output
-  --memory                 Measure memory usage and display data table
-  --memory-realtime        Log memory usage in realtime
-  --performance            Measure count and running time of key functions and display stats table
-  --performance-fn [name]  Measure only function [name]
-  --duration               Print total running time (zero overhead, no instrumentation)
-  --trace                  Show trace output
-  --trace-dependency [name] Show files that import the named dependency
-  --trace-export [name]    Show trace output for named export(s)
-  --trace-file [file]      Show trace output for exports in file
+  -d, --debug                  Show debug output
+      --memory                 Measure memory usage and display data table
+      --memory-realtime        Log memory usage in realtime
+      --performance            Measure count and running time of key functions and display stats table
+      --performance-fn [name]  Measure only function [name]
+      --duration               Print total running time (zero overhead, no instrumentation)
+      --trace                  Show trace output
+      --trace-dependency [name]  Show files that import the named dependency
+      --trace-export [name]    Show trace output for named export(s)
+      --trace-file [file]      Show trace output for exports in file
 
 (1) Issue types: files, dependencies, unlisted, unresolved, exports, nsExports, types, nsTypes, enumMembers, namespaceMembers, duplicates, catalog
 (2) Fixable issue types: dependencies, exports, types, files, catalog
@@ -99,7 +99,6 @@ export default function parseCLIArgs() {
       'allow-remove-files': { type: 'boolean' },
       help: { type: 'boolean', short: 'h' },
       include: { type: 'string', multiple: true },
-
       'include-entry-exports': { type: 'boolean' },
       'max-issues': { type: 'string' },
       'max-show-issues': { type: 'string' },
