e3e5bc94d5f7b6ffdbc89b18d7c8d5acbb5a9008
Lars Kappert <lars@webpro.nl>
2026-05-30 08:42:21 +0200

    Add nuxt no-root-tsconfig fixture guarding alias resolution

---
+++ packages/knip/fixtures/plugins/nuxt-no-root-tsconfig/app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { greeting } from '~/utils/greeting';
+
+const message = ref(greeting);
+</script>
+
+<template>
+  <div>{{ message }}</div>
+</template>
