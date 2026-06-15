e6cc533a8772cdc4aba93cac376d214fb2fb5a58
Joshua Melville <joshua.melville@northwestern.edu>
2026-06-15 11:26:57 +0200

    fix(capacitor): detect iOS platform with Swift Package Manager (#1787)

---
+++ packages/knip/fixtures/plugins/capacitor-spm/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.company.name',
+  appName: 'name',
+  plugins: {},
+};
+
+export default config;
