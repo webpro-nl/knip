db1c7782c6d02c3d96d494e1f8614c67c6c12cf6
Remco Haszing <remcohaszing@gmail.com>
2026-07-06 10:50:24 +0200

    Fix broken test

--- packages/knip/fixtures/plugins/prettier/prettier.config.js
+++ packages/knip/fixtures/plugins/prettier/prettier.config.js
@@ -1,4 +1,4 @@
 export default {
   plugins: ['prettier-plugin-xml', import.meta.resolve('prettier-plugin-astro')],
-  overrides: [{ options: 'prettier-plugin-java' }],
+  overrides: [{ options: { plugins: ['prettier-plugin-java'] } }],
 };
