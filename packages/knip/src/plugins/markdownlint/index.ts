fa6eb8a439777ba811f4d3b190857137e72f3754
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-07-16 18:26:17 -0400

    feat: markdownlint

--- packages/knip/src/plugins/markdownlint/index.ts
+++ packages/knip/src/plugins/markdownlint/index.ts
@@ -5,25 +5,41 @@ import { getArgumentValues } from './helpers.ts';
 import type { MarkdownlintConfig } from './types.ts';
 
 // https://github.com/igorshubovych/markdownlint-cli
+// https://github.com/DavidAnson/markdownlint-cli2
 
 const title = 'markdownlint';
 
-const enablers = ['markdownlint-cli'];
+const enablers = ['markdownlint-cli', 'markdownlint-cli2'];
 
 const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependencies, enablers);
 
-const config = ['.markdownlint.{json,jsonc}', '.markdownlint.{yml,yaml}'];
+const config = ['.markdownlint-cli2.{jsonc,yaml,cjs,mjs}', '.markdownlint.{json,jsonc,yaml,yml,cjs,mjs}'];
 
 const resolveConfig: ResolveConfig<MarkdownlintConfig> = (config, options) => {
   const { manifest } = options;
-  const extend = config?.extends ? [config.extends] : [];
+  const dependencies: string[] = [];
+  if (config?.extends) dependencies.push(config.extends);
+  if (config?.config?.extends) dependencies.push(config.config.extends);
+  for (const customRule of config?.customRules ?? []) {
+    if (typeof customRule === 'string') dependencies.push(customRule);
+    else if (Array.isArray(customRule)) {
+      for (const rule of customRule) {
+        if (typeof rule === 'string') dependencies.push(rule);
+      }
+    }
+  }
+  for (const modules of [config?.markdownItPlugins, config?.outputFormatters]) {
+    for (const module of modules ?? []) {
+      if (Array.isArray(module) && typeof module[0] === 'string') dependencies.push(module[0]);
+    }
+  }
   const scripts = manifest?.scripts
     ? Object.values(manifest.scripts).filter((script): script is string => typeof script === 'string')
     : [];
   const uses = scripts
     .filter(script => script.includes('markdownlint '))
     .flatMap(script => getArgumentValues(script, / (--rules|-r)[ =]([^ ]+)/g));
-  return [...extend, ...uses].map(id => toDependency(id));
+  return [...dependencies, ...uses].map(id => toDependency(id));
 };
 
 const plugin: Plugin = {
