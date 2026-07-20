323b96f55b38b53f5e4648d72fecd968fae35147
James <5511220+Zamiell@users.noreply.github.com>
2026-07-20 04:19:25 -0400

    feat: markdownlint-cli2 (#1887)
    Co-authored-by: Lars Kappert <lars@webpro.nl>

--- packages/knip/src/plugins/markdownlint/types.ts
+++ packages/knip/src/plugins/markdownlint/types.ts
@@ -1,3 +1,9 @@
 export type MarkdownlintConfig = {
+  config?: {
+    extends?: string;
+  };
+  customRules?: unknown[];
   extends?: string;
+  markdownItPlugins?: unknown[];
+  outputFormatters?: unknown[];
 };
