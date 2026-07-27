d29cb469ad712822ecf1eed0afd2aa5a95872a9c
Lars Kappert <lars@webpro.nl>
2026-07-27 22:52:30 +0200

    Resolve config file passed to stryker run command

--- packages/knip/src/plugins/stryker/index.ts
+++ packages/knip/src/plugins/stryker/index.ts
@@ -1,5 +1,6 @@
+import type { Args } from '../../types/args.ts';
 import type { IsPluginEnabled, Plugin, ResolveConfig } from '../../types/config.ts';
-import { toDeferResolve } from '../../util/input.ts';
+import { toConfig, toDeferResolve } from '../../util/input.ts';
 import { hasDependency } from '../../util/plugin.ts';
 import type { StrykerConfig } from './types.ts';
 
@@ -11,7 +12,7 @@ const enablers = ['@stryker-mutator/core'];
 
 const isEnabled: IsPluginEnabled = ({ dependencies }) => hasDependency(dependencies, enablers);
 
-const config = ['?(.)stryker.{conf,config}.{js,mjs,cjs,json,mts,cts,ts}'];
+const config = ['?(.)stryker.{conf,config}.{js,mjs,cjs,json}'];
 
 const resolveConfig: ResolveConfig<StrykerConfig> = localConfig => {
   const runners = localConfig.testRunner ? [`@stryker-mutator/${localConfig.testRunner}-runner`] : [];
@@ -23,12 +24,18 @@ const resolveConfig: ResolveConfig<StrykerConfig> = localConfig => {
   return [...runners, ...checkers, ...plugins].map(id => toDeferResolve(id));
 };
 
+const args: Args = {
+  boolean: ['allowEmpty', 'disableBail', 'dryRunOnly', 'force', 'ignoreStatic', 'incremental', 'inPlace'],
+  resolveInputs: parsed => (parsed._[0] === 'run' && parsed._[1] ? [toConfig('stryker', parsed._[1])] : []),
+};
+
 const plugin: Plugin = {
   title,
   enablers,
   isEnabled,
   config,
   resolveConfig,
+  args,
 };
 
 export default plugin;
