1f77617e4f6888598694107182ac3f7199b6de3b
Lars Kappert <lars@webpro.nl>
2026-08-26 17:18:43 +0200

    Report suppressed issues in JSON and without a TTY

--- packages/knip/src/reporters/symbols.ts
+++ packages/knip/src/reporters/symbols.ts
@@ -43,7 +43,7 @@ export default (options: ReporterOptions) => {
     console.log('✂️  Excellent, Knip found no issues.');
   }
 
-  if (isShowProgress && options.suppressedCount && options.suppressedCount > 0) {
+  if (options.suppressedCount && options.suppressedCount > 0) {
     const parts = [`${options.suppressedCount} suppressed`];
     if (options.expiredCount && options.expiredCount > 0) parts.push(`${options.expiredCount} expired`);
     console.log(dim(`(${parts.join(', ')})`));
