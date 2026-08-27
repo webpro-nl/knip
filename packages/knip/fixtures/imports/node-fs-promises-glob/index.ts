5339651ff830516ad6e0d85b0a276ba5ee84f6cf
DreamLong <chessmrbeaston@gmail.com>
2026-08-27 15:56:25 +0200

    Update index.ts

--- packages/knip/fixtures/imports/node-fs-promises-glob/index.ts
+++ packages/knip/fixtures/imports/node-fs-promises-glob/index.ts
@@ -1,8 +1,22 @@
 import { glob } from 'node:fs/promises';
 import * as fsPromises from 'node:fs/promises';
+import './nested/index.ts';
+import path from 'node:path';
 
 const { glob: requireGlob } = require('node:fs/promises');
+const { 'glob': literalKeyGlob } = require('node:fs/promises');
+const staticCwd = './migrations';
+const dynamicPattern = process.env.PATTERN ?? 'dynamic/*.ts';
 
 export const migrationFiles = glob('*.ts', { cwd: './migrations' });
 export const namespaceMigrationFiles = fsPromises.glob('*.ts', { cwd: './migrations' });
 export const commonJsMigrationFiles = requireGlob('*.ts', { cwd: './migrations' });
+export const literalKeyMigrationFiles = literalKeyGlob('*.ts', { cwd: './migrations' });
+export const computedCwdMigrationFiles = glob('*.ts', { ['cwd']: './migrations' });
+export const rootFiles = glob('root/*.ts');
+export const emptyOptionsRootFiles = glob('root/*.ts', {});
+export const staticArrayFiles = glob(['root/*.ts', 'migrations/*.ts']);
+export const dynamicCwdFiles = glob('dynamic/*.ts', { cwd: process.cwd() });
+export const dynamicResolvedCwdFiles = glob('*.ts', { cwd: path.resolve('migrations') });
+export const identifierCwdFiles = glob('*.ts', { cwd: staticCwd });
+export const dynamicPatternFiles = glob([dynamicPattern, 'dynamic/*.ts']);
